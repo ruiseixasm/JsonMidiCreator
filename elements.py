ae27426ae1e2aa2f05f67c723a35a1fbb42c11c4
Rui Monteiro <ruiseixas@yahoo.com>
2024-07-20 04:18:35 +0100

    Improved Tempo class

--- elements.py
+++ elements.py
@@ -36,7 +36,50 @@ class Tempo:
         ...
 
     def getPlayList(self, staff, time_signature):
-        ...
+        pulses_per_note = 4 * self._pulses_per_quarternote
+        pulses_per_beat = pulses_per_note / time_signature._beats_per_note
+        staff_pulses = pulses_per_beat * time_signature._beats_per_measure * staff._measures
+        staff_duration_ms = (60 * 1000 / self._bpm) * time_signature._beats_per_measure * staff._measures
+
+        # System Real-Time Message         Status Byte 
+        # ------------------------         -----------
+        # Timing Clock                         F8
+        # Start Sequence                       FA
+        # Continue Sequence                    FB
+        # Stop Sequence                        FC
+        # Active Sensing                       FE
+        # System Reset                         FF
+
+        play_list = [
+                {
+                    "time_ms": 0,
+                    "midi_message": {
+                        "status_byte": 0xFA
+                    }
+                }
+            ]
+
+        for staff_pulse in range(1, staff_pulses):
+            play_list.append(
+                {
+                    "time_ms": staff_duration_ms * staff_pulse / staff_pulses,
+                    "midi_message": {
+                        "status_byte": 0xF8
+                    }
+                }
+            )
+
+        play_list.append(
+            {
+                "time_ms": staff_duration_ms,
+                "midi_message": {
+                    "status_byte": 0xFC
+                }
+            }
+        )
+
+        return play_list
+
 
 class Quantization:
         
@@ -52,6 +95,18 @@ class Quantization:
 class Creator:
 
     def __init__(self):
+        pass
+
+    def addDevice(play_list, devicename):
+        pass
+
+    def removeDevice(play_list, devicename):
+        pass
+
+    def printDevices(play_list):
+        pass
+
+    def setChannel(play_list, channel):
         pass
 
     def saveJson(self, json_list, filename):
