c410fcf9095f34ee1777c1640757c31ee1567137
Rui Monteiro <ruiseixas@yahoo.com>
2024-07-24 10:26:22 +0100

    Ctypes fully implemented on creator

--- creator.py
+++ creator.py
@@ -3,6 +3,46 @@ import platform
 import os
 import ctypes
 
+# Determine the directory of the current Python file
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Determine the operating system
+current_os = platform.system()
+
+# Define the name of the shared library based on the operating system
+if current_os == "Windows":
+    lib_name = 'libMidiJsonPlayer_ctypes.dll'
+elif current_os == "Darwin":  # macOS
+    lib_name = 'libMidiJsonPlayer_ctypes.dylib'
+else:  # Assume Linux/Unix
+    lib_name = 'libMidiJsonPlayer_ctypes.so'
+
+# Construct the full path to the library
+lib_path = os.path.join(script_dir, 'lib', lib_name)
+
+# Check if the library file exists
+if not os.path.isfile(lib_path):
+    raise FileNotFoundError(f"COULD NOT FIND THE LIBRARY FILE: {lib_path}")
+else:
+    # Print the library path for debugging
+    print(f"Library FOUND in: {lib_path}")
+    try:
+        # Load the shared library
+        lib = ctypes.CDLL(lib_path)
+        # Define the argument and return types for the C function
+        lib.PlayList_ctypes.argtypes = [ctypes.c_char_p]
+        lib.PlayList_ctypes.restype = ctypes.c_int
+
+    except FileNotFoundError:
+        print(f"Could not find the library file: {lib_path}")
+    except OSError as e:
+        print(f"An error occurred while loading the library: {e}")
+    except AttributeError as e:
+        print(f"An error occurred while accessing the function: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
 class Configuration:
     ...
 
@@ -11,27 +51,8 @@ class Configuration:
 class PlayList:
 
     def __init__(self):
-        # Determine the operating system
-        self._current_os = platform.system()
+        ...
 
-        # Load the shared library based on the operating system
-        if self._current_os == "Windows":
-            self._lib_path = os.path.abspath('./libMidiJsonPlayer_ctypes.dll')
-        elif self._current_os == "Darwin":  # macOS
-            self._lib_path = os.path.abspath('./libMidiJsonPlayer_ctypes.dylib')
-        else:  # Assume Linux/Unix
-            self._lib_path = os.path.abspath('./libMidiJsonPlayer_ctypes.so')
-
-        # Check if the library file exists
-        if not os.path.isfile(self._lib_path):
-            raise FileNotFoundError(f"Could not find the library file: {self._lib_path}")
-            self._lib = None
-        else:
-            print(f"Found the library file: {self._lib_path}\n")
-            # Load the shared library
-            self._lib = ctypes.CDLL(self._lib_path)
-            # Define the argument and return types for the C function
-            self._lib.PlayList_ctypes.argtypes = [ctypes.c_char_p]
 
     def removeDevice(self, play_list, devicename):
         pass
@@ -44,15 +65,25 @@ class PlayList:
 
     def playPlayList(self, play_list):
 
-        if self._lib is not None:
-            json_file_dict = {
-                    "filetype": "Midi Json Player",
-                    "content": play_list
-                }
-            # Convert Python dictionary to JSON string
-            json_str = json.dumps([ json_file_dict ])
+        json_file_dict = {
+                "filetype": "Midi Json Player",
+                "content": play_list
+            }
+        # Convert Python dictionary to JSON string
+        json_str = json.dumps([ json_file_dict ])
+
+        try:
             # Call the C++ function with the JSON string
-            self._lib.PlayList_ctypes(json_str.encode('utf-8'))
+            lib.PlayList_ctypes(json_str.encode('utf-8'))
+        except FileNotFoundError:
+            print(f"Could not find the library file: {lib_path}")
+        except OSError as e:
+            print(f"An error occurred while loading the library: {e}")
+        except AttributeError as e:
+            print(f"An error occurred while accessing the function: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred when calling the function 'PlayList_ctypes': {e}")
+
 
     def saveJson(self, json_list, filename):
         pass
