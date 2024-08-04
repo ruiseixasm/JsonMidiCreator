5ca7018d3997e3dbcff3a1b40e14572ebc1b3b0a
Rui Monteiro <ruiseixas@yahoo.com>
2024-08-04 19:19:40 +0100

    File saving, loading and playing methods are now global creator functions

--- JsonMidiCreator.py
+++ JsonMidiCreator.py
@@ -1,4 +1,5 @@
-'''JsonMidiCreator - Json Midi Creator is intended to be used
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
 in conjugation with the Json Midi Player to Play composed Elements
 Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
 This library is free software; you can redistribute it and/or
@@ -8,7 +9,10 @@ version 2.1 of the License, or (at your option) any later version.
 This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-Lesser General Public License for more details.'''
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
 from staff import *
 from operand import *
 from element import *
