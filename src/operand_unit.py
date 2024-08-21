c9b65c762c3f89151f6f413a40369f1b1a61082e
Rui Monteiro <ruiseixas@yahoo.com>
2024-08-21 07:22:57 +0100

    Implementing Add and Sub for Scale

--- src/operand_unit.py
+++ src/operand_unit.py
@@ -279,6 +279,22 @@ class Scale(Unit):
                     interval += 1
         return chromatic_transposition
 
+    # CHAINABLE OPERATIONS
+
+    def __add__(self, operand: Operand) -> 'Scale':
+        self_copy = self.copy()
+        match operand:
+            case int():         self_copy << self._key_note + operand
+            case _:             return super().__add__(operand)
+        return self_copy
+
+    def __sub__(self, operand: Operand) -> 'Scale':
+        self_copy = self.copy()
+        match operand:
+            case int():         self_copy << self._key_note - operand
+            case _:             return super().__sub__(operand)
+        return self_copy
+
         # 1 - First, 2 - Second, 3 - Third, 4 - Fourth, 5 - Fifth, 6 - Sixth, 7 - Seventh,
         # 8 - Eighth, 9 - Ninth, 10 - Tenth, 11 - Eleventh, 12 - Twelfth, 13 - Thirteenth,
         # 14 - Fourteenth, 15 - Fifteenth, 16 - Sixteenth, 17 - Seventeenth, 18 - Eighteenth,
