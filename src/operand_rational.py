734d35a1d41c4dc4a73f57312bab56183b2eb88b
Rui Monteiro <ruiseixas@yahoo.com>
2024-10-08 09:46:09 +0100

    Examples clean code

--- src/operand_rational.py
+++ src/operand_rational.py
@@ -447,11 +447,11 @@ class Measure(TimeUnit):
             # case Measure():
             #     self._rational = operand % o.Operand() % Fraction()
             case Beat():
-                self._rational = operand % Fraction() / (os.staff % od.DataSource( BeatsPerMeasure() ) % Fraction())
+                self._rational = operand._rational / (os.staff % od.DataSource( BeatsPerMeasure() ) % Fraction())
             case Step():
-                self._rational = operand % Fraction() / (os.staff % od.DataSource( StepsPerMeasure() ) % Fraction())
+                self._rational = operand._rational / (os.staff % od.DataSource( StepsPerMeasure() ) % Fraction())
             case NoteValue():
-                self._rational = operand % Fraction() / (os.staff % od.DataSource( NotesPerMeasure() ) % Fraction())
+                self._rational = operand._rational / (os.staff % od.DataSource( NotesPerMeasure() ) % Fraction())
             case _: super().__lshift__(operand)
         return self
 
