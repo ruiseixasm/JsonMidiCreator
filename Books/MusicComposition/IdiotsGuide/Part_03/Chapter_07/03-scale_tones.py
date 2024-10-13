3cd25492f725cd61bca9e164dfd07e429f5f29f0
Rui Monteiro <ruiseixas@yahoo.com>
2024-10-13 10:50:07 +0100

    Book example

--- Books/MusicComposition/IdiotsGuide/Part_03/Chapter_07/03-scale_tones.py
+++ Books/MusicComposition/IdiotsGuide/Part_03/Chapter_07/03-scale_tones.py
@@ -21,4 +21,10 @@ if src_path not in sys.path:
 
 from JsonMidiCreator import *
 
+staff << KeySignature("#")
+scale_tones = Note("G", 1/1) * 4 + (0, 2, 0, -1)
+scale_tones >> Play()
 
+staff << KeySignature("bb")
+stable_unstable = Note("B", 1/1) * 4 + ((3 - 1), (1 - 1), -1, (1 - 1))
+stable_unstable >> Play()
