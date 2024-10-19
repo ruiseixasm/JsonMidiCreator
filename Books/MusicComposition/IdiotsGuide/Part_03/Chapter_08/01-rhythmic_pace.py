4722dfe806bc2c6f8d39a3001ee70926aee0613e
Rui Monteiro <ruiseixas@yahoo.com>
2024-10-19 21:33:51 +0100

    Concluded Book example 01

--- Books/MusicComposition/IdiotsGuide/Part_03/Chapter_08/01-rhythmic_pace.py
+++ Books/MusicComposition/IdiotsGuide/Part_03/Chapter_08/01-rhythmic_pace.py
@@ -24,21 +24,21 @@ from JsonMidiCreator import *
 staff << KeySignature("bbb")
 
 simple_phrase: Sequence = Note("B") * 3 + Nth(2)**1 >> Link(True)
-simple_phrase >> Rest() >> Play()
+# simple_phrase >> Rest() >> Play()
 
 dotted_quarter: Sequence = simple_phrase.copy() << Foreach(Dotted(1/4), 1/8, 1/2) >> Stack() >> Link(True)
-dotted_quarter >> Rest() >> Play()
+# dotted_quarter >> Rest() >> Play()
 
 off_beat: Sequence = simple_phrase.copy() << Nth(2)**Position(NoteValue(1/8)) >> Link(True)
-off_beat >> Rest() >> Play()
+# off_beat >> Rest() >> Play()
 
 speeding_up: Sequence = dotted_quarter.copy() - Nth(2, 3)**Position(1/4) >> Link(True)
-speeding_up >> Rest() >> Play()
+# speeding_up >> Rest() >> Play()
 
 staff << KeySignature("#")
 
 original_phrase: Sequence = Note("B") * 5 + Foreach(2, -1, 0, 2, 1) >> Link(True)
-original_phrase >> Rest() >> Play()
+# original_phrase >> Rest() >> Play()
 
 variation_a: Sequence = original_phrase.copy() << Equal(Measure(0))**NoteValue(1/8)
 (variation_a | Measure(0)) >> Stack()
@@ -50,7 +50,7 @@ variation_d: Sequence = original_phrase.copy() << Equal(Measure(0))**Foreach(1/4
 
 variation_e: Sequence = Rest() + (original_phrase.copy() << Equal(Measure(0))**NoteValue(1/8)) >> Stack()
 
-variation_f: Sequence = ((original_phrase | Measure(0)) << Gate(1)) + original_phrase << Equal(Measure(0))**NoteValue(1/8)
-variation_f = Rest() + variation_f + Note("D", 5, 1/8, Position(Beat(4) - NoteValue(1/8))) >> Sort()
+variation_f: Sequence = ((original_phrase | Measure(0) | Less(Beat(3))).copy() << Gate(1)) + original_phrase << Equal(Measure(0))**NoteValue(1/8) >> Sort()
+variation_f = Rest(1/8) + variation_f
 
 variation_a >> variation_b >> variation_c >> variation_d >> variation_e >> variation_f >> Rest() >> Play()
