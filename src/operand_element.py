abe224b096b26e4dad8cc4f9f1e9e9f3ed46aee5
Rui Monteiro <ruiseixas@yahoo.com>
2024-10-25 06:48:34 +0100

    Adjusted Chord class to use Degree instead of its parameters

--- src/operand_element.py
+++ src/operand_element.py
@@ -752,10 +752,7 @@ class Chord(KeyScale):
         super().__init__()
         self._size: ou.Size             = ou.Size(3)
         self._inversion: ou.Inversion   = ou.Inversion(0)
-        self._sharp: ou.Sharp           = ou.Sharp(0)
-        self._flat: ou.Flat             = ou.Flat(0)
-        self._dominant: ou.Dominant     = ou.Dominant(0)
-        self._diminished: ou.Diminished = ou.Diminished(0)
+        self._degree: ou.Degree         = ou.Degree()
         self._sus2: ou.Sus2             = ou.Sus2(0)
         self._sus4: ou.Sus4             = ou.Sus4(0)
         if len(parameters) > 0:
@@ -778,19 +775,13 @@ class Chord(KeyScale):
                 match operand % o.Operand():
                     case ou.Size():         return self._size
                     case ou.Inversion():    return self._inversion
-                    case ou.Sharp():        return self._sharp
-                    case ou.Flat():         return self._flat
-                    case ou.Dominant():     return self._dominant
-                    case ou.Diminished():   return self._diminished
+                    case ou.Degree():       return self._degree
                     case ou.Sus2():         return self._sus2
                     case ou.Sus4():         return self._sus4
                     case _:                 return super().__mod__(operand)
             case ou.Size():         return self._size.copy()
             case ou.Inversion():    return self._inversion.copy()
-            case ou.Sharp():        return self._sharp.copy()
-            case ou.Flat():         return self._flat.copy()
-            case ou.Dominant():     return self._dominant.copy()
-            case ou.Diminished():   return self._diminished.copy()
+            case ou.Degree():       return self._degree.copy()
             case ou.Sus2():         return self._sus2.copy()
             case ou.Sus4():         return self._sus4.copy()
             case _:                 return super().__mod__(operand)
@@ -802,10 +793,7 @@ class Chord(KeyScale):
                 return super().__eq__(other_operand) \
                     and self._size          == other_operand % od.DataSource( ou.Size() ) \
                     and self._inversion     == other_operand % od.DataSource( ou.Inversion() ) \
-                    and self._sharp         == other_operand % od.DataSource( ou.Sharp() ) \
-                    and self._flat          == other_operand % od.DataSource( ou.Flat() ) \
-                    and self._dominant      == other_operand % od.DataSource( ou.Dominant() ) \
-                    and self._diminished    == other_operand % od.DataSource( ou.Diminished() ) \
+                    and self._degree        == other_operand % od.DataSource( ou.Degree() ) \
                     and self._sus2          == other_operand % od.DataSource( ou.Sus2() ) \
                     and self._sus4          == other_operand % od.DataSource( ou.Sus4() )
             case _:
@@ -833,15 +821,15 @@ class Chord(KeyScale):
                 if self._sus4:
                     key_note_nth += 1   # cancels out if both sus2 and sus4 are set to true
             transposition = modulated_scale.transposition(key_note_nth)
-            if self._flat:
+            if self._degree._flat:
                 transposition -= 1
-            if self._sharp:
+            if self._degree._sharp:
                 transposition += 1   # cancels out if both flat and sharp are set to true
             if key_note_nth == 7:   # Seventh
-                if self._dominant:
+                if self._degree._dominant:
                     transposition -= 1
             if key_note_nth == 3 or key_note_nth == 5:   # flattens Third and Fifth
-                if self._diminished:
+                if self._degree._diminished:
                     transposition -= 1   # cancels out if both dominant and diminished are set to true
             chord_key_notes.append(root_key_note + float(transposition))
 
@@ -870,10 +858,7 @@ class Chord(KeyScale):
         element_serialization = super().getSerialization()
         element_serialization["parameters"]["size"]         = self._size % od.DataSource( int() )
         element_serialization["parameters"]["inversion"]    = self._inversion % od.DataSource( int() )
-        element_serialization["parameters"]["sharp"]        = self._sharp % od.DataSource( int() )
-        element_serialization["parameters"]["flat"]         = self._flat % od.DataSource( int() )
-        element_serialization["parameters"]["dominant"]     = self._dominant % od.DataSource( int() )
-        element_serialization["parameters"]["diminished"]   = self._diminished % od.DataSource( int() )
+        element_serialization["parameters"]["degree"]       = self._degree.getSerialization()
         element_serialization["parameters"]["sus2"]         = self._sus2 % od.DataSource( int() )
         element_serialization["parameters"]["sus4"]         = self._sus4 % od.DataSource( int() )
         return element_serialization
@@ -883,16 +868,12 @@ class Chord(KeyScale):
     def loadSerialization(self, serialization: dict):
         if isinstance(serialization, dict) and ("class" in serialization and serialization["class"] == self.__class__.__name__ and "parameters" in serialization and
             "inversion" in serialization["parameters"] and "size" in serialization["parameters"] and 
-            "sharp" in serialization["parameters"] and "flat" in serialization["parameters"] and "dominant" in serialization["parameters"] and
-            "diminished" in serialization["parameters"] and "sus2" in serialization["parameters"] and "sus4" in serialization["parameters"]):
+            "degree" in serialization["parameters"] and "sus2" in serialization["parameters"] and "sus4" in serialization["parameters"]):
 
             super().loadSerialization(serialization)
             self._size          = ou.Size()         << od.DataSource( serialization["parameters"]["size"] )
             self._inversion     = ou.Inversion()    << od.DataSource( serialization["parameters"]["inversion"] )
-            self._sharp         = ou.Sharp()        << od.DataSource( serialization["parameters"]["sharp"] )
-            self._flat          = ou.Flat()         << od.DataSource( serialization["parameters"]["flat"] )
-            self._dominant      = ou.Dominant()     << od.DataSource( serialization["parameters"]["dominant"] )
-            self._diminished    = ou.Diminished()   << od.DataSource( serialization["parameters"]["diminished"] )
+            self._degree        = ou.Degree().loadSerialization( serialization["parameters"]["degree"] )
             self._sus2          = ou.Sus2()         << od.DataSource( serialization["parameters"]["sus2"] )
             self._sus4          = ou.Sus4()         << od.DataSource( serialization["parameters"]["sus4"] )
         return self
@@ -904,10 +885,7 @@ class Chord(KeyScale):
                 match operand % o.Operand():
                     case ou.Size():                 self._size = operand % o.Operand()
                     case ou.Inversion():            self._inversion = operand % o.Operand()
-                    case ou.Sharp():                self._sharp = operand % o.Operand()
-                    case ou.Flat():                 self._flat = operand % o.Operand()
-                    case ou.Dominant():             self._dominant = operand % o.Operand()
-                    case ou.Diminished():           self._diminished = operand % o.Operand()
+                    case ou.Degree():               self._degree = operand % o.Operand()
                     case ou.Sus2():                 self._sus2 = operand % o.Operand()
                     case ou.Sus4():                 self._sus4 = operand % o.Operand()
                     case _:                         super().__lshift__(operand)
@@ -915,45 +893,25 @@ class Chord(KeyScale):
                 super().__lshift__(operand)
                 self._size          << operand._size
                 self._inversion     << operand._inversion
-                self._sharp         << operand._sharp
-                self._flat          << operand._flat
-                self._dominant      << operand._dominant
-                self._diminished    << operand._diminished
+                self._degree        << operand._degree
                 self._sus2          << operand._sus2
                 self._sus4          << operand._sus4
             case ou.Size():                 self._size << operand
             case ou.Inversion():            self._inversion << operand
-            case ou.Sharp():
-                self._sharp << operand
-                if self._sharp:         # mutual exclusive
-                    self._flat << False
-            case ou.Flat():
-                self._flat << operand
-                if self._flat:          # mutual exclusive
-                    self._sharp << False
-            case ou.Dominant():
-                self._dominant << operand
-                if self._dominant:      # mutual exclusive
-                    self._diminished << False
-                    self._sus2 << False
-                    self._sus4 << False
-            case ou.Diminished():
-                self._diminished << operand
-                if self._diminished:    # mutual exclusive
-                    self._dominant << False
+            case ou.Degree() | ou.Sharp() | ou.Flat() | ou.Dominant() | ou.Diminished():
+                self._degree << operand
+                if self._degree._dominant or self._degree._diminished:  # mutual exclusive
                     self._sus2 << False
                     self._sus4 << False
             case ou.Sus2():
                 self._sus2 << operand
                 if self._sus2:          # mutual exclusive
-                    self._dominant << False
-                    self._diminished << False
+                    self._degree << ou.Dominant(0) << ou.Diminished(0)
                     self._sus4 << False
             case ou.Sus4():
                 self._sus4 << operand
                 if self._sus4:          # mutual exclusive
-                    self._dominant << False
-                    self._diminished << False
+                    self._degree << ou.Dominant(0) << ou.Diminished(0)
                     self._sus2 << False
             case _: super().__lshift__(operand)
         return self
