e12f7057f34e5d96ede869a72e5b1d8f8836d461
Rui Monteiro <ruiseixas@yahoo.com>
2024-11-01 15:23:34 +0000

    Book examples

---
+++ Books/MusicComposition/IdiotsGuide/Part_04/Chapter_12/02-writing_phrases.py
@@ -0,0 +1,34 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+import sys
+import os
+src_path = os.path.join(os.path.dirname(__file__), '../../../../..', 'src')
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from JsonMidiCreator import *
+
+
+staff << "b" << 140
+K % str() >> Print()    # Returns the tonic key (I)
+
+phrase_1 = N * 6 << whole << Nth(1, 2, 3, 4)**Foreach(dotted_half, quarter, half, half) >> S >> Tie()
+phrase_1 << Foreach("iii", "ii", "iii", "IV", "iii", "I")**Degree()
+phrase_2 = phrase_1 % Copy() << Foreach(-3, -2, 1, 2, 3, 3)**Degree()
+phrase_4 = phrase_1 % Copy() << Foreach(3, 4, 3, 4, 3, 3)**Degree()
+phrase_3 = phrase_1 % Equal(M1, M2) % Copy() \
+    + (N * 4 << half << Nth(1, 2)**Foreach(dotted_half, quarter) << Foreach(5, 6, 5, 4)**Degree()) >> S
+(phrase_1, phrase_2, phrase_3, phrase_4, R) >> P
