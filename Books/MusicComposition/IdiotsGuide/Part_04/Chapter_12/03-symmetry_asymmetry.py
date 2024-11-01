724f05299189be56d263edbb9bf615ff86fcb69f
Rui Monteiro <ruiseixas@yahoo.com>
2024-11-01 17:28:03 +0000

    Book example

---
+++ Books/MusicComposition/IdiotsGuide/Part_04/Chapter_12/03-symmetry_asymmetry.py
@@ -0,0 +1,39 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+import sys
+import os
+src_path = os.path.join(os.path.dirname(__file__), '../../../../..', 'src')
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from JsonMidiCreator import *
+
+
+staff << 120
+K % str() >> Print()    # Returns the tonic key (I)
+
+phrase_1 = N * 7 >> LJ
+phrase_1 % Length() >> Print(0)
+phrase_2 = N * 5 >> LJ
+phrase_2 % Length() >> Print(0)
+symmetrical = \
+    (phrase_1 % Copy()  << Foreach(8, 5, 6, 7, 8, 9, 8)**Degree()   >> Slur()) + \
+    (phrase_2 % Copy()  << Foreach(7, 8, 7, 6, 7)**Degree()         >> Slur()) + \
+    (phrase_1 % Copy()  << Foreach(8, 5, 6, 7, 8, 9, 10)**Degree()  >> Slur()) >> S
+symmetrical += symmetrical % Equal(M3, M4) % Copy() + 3
+symmetrical >> S
+symmetrical % Length() >> Print(0)
+symmetrical >> R >> P
