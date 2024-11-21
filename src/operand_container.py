2e3f412c7d2ec154fb5ba271fed25be972506757
Rui Monteiro <ruiseixas@yahoo.com>
2024-11-21 13:17:25 +0000

    Improved stack method of Sequence class

--- src/operand_container.py
+++ src/operand_container.py
@@ -479,15 +479,15 @@ class Sequence(Container):  # Just a container of Elements
 
     def stack(self) -> 'Sequence':
         import operand_element as oe
-        # Starts by sorting the self Elements list accordingly to their Tracks (all data is an Element)
-        sorted_elements: list[oe.Element] = [
+        # Starts by sorting the self Elements list accordingly to their Tracks (all data is a Stackable Element)
+        sorted_elements: list[oe.Stackable] = [
                 single_data._data
                 for single_data in self._datasource_list
                 if isinstance(single_data._data, oe.Stackable)
             ]
         sorted_elements = sorted(sorted_elements, key=lambda x: (id(x._track._track_data)))
         for single_element_i in range(len(sorted_elements)):
-            single_element: oe.Element = sorted_elements[single_element_i]
+            single_element: oe.Stackable = sorted_elements[single_element_i]
             if single_element_i > 0 and id(sorted_elements[single_element_i]._track._track_data) == id(sorted_elements[single_element_i - 1]._track._track_data):
                 single_element._position = sorted_elements[single_element_i - 1]._position + sorted_elements[single_element_i - 1]._length  # Stacks on Element Length
             else:
