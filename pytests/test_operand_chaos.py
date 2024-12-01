1a5861361dbc5c6de97294d3e981e5ee61e013a5
Rui Monteiro <ruiseixas@yahoo.com>
2024-12-01 01:24:15 +0000

    Created the Chaos pytest file

---
+++ pytests/test_operand_chaos.py
@@ -0,0 +1,51 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+import sys
+import os
+src_path = os.path.join(os.path.dirname(__file__), '..', 'src')
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from JsonMidiCreator import *
+
+
+# Run the tests with 'pytest tests\python_functions.py' on windows
+# Run the tests with 'pytest tests/python_functions.py' on linux
+
+from io import StringIO
+import pytest
+import sys
+
+
+
+def test_chaos_mod():
+
+    # Perform the operation
+    sin_x = SinX()
+
+    sin_x_1000 = sin_x * 1000 >> Copy()
+    sin_x_1001 = sin_x * 1 >> Copy()
+
+    assert sin_x_1000 != sin_x_1001
+    assert sin_x_1001 == sin_x
+    sin_x.reset()
+    assert sin_x_1001 != sin_x
+    sin_x * 1000
+    assert sin_x_1000 == sin_x
+    assert sin_x_1000 * 1 == sin_x_1001
+
+
+
