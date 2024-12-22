8570688f252088ad00b1aa6ff99d605a7689f9a9
Rui Monteiro <ruiseixas@yahoo.com>
2024-12-22 18:43:38 +0000

    Completed Truncation example

--- Books/MusicComposition/IdiotsGuide/Part_04/Chapter_13/12-truncation.py
+++ Books/MusicComposition/IdiotsGuide/Part_04/Chapter_13/12-truncation.py
@@ -29,7 +29,7 @@ K % str() >> Print()    # Returns the tonic key (I)
 motif = N * 6 << Foreach(quarter, eight, eight, dotted_quarter, eight, whole) >> S
 motif << Foreach(-3, 1, 2, 3, 2, -3)**Degree()
 
-truncated_motif = motif.copy() % LessEqual(Measure(1/2)) >> Link()
+truncated_motif = motif.copy() % LessEqual(Position(Beat(2))) >> Link()
 
 # Where the Variation pitch is generated (Foreach does iteration contrary to Subject)
 varying_rhythm = motif >> truncated_motif
