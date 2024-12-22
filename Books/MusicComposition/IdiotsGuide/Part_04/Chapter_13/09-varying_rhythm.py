286923d22da5c79618ffdb541c3c9c997a980bae
Rui Monteiro <ruiseixas@yahoo.com>
2024-12-22 18:09:08 +0000

    Translocation works on Position

--- Books/MusicComposition/IdiotsGuide/Part_04/Chapter_13/09-varying_rhythm.py
+++ Books/MusicComposition/IdiotsGuide/Part_04/Chapter_13/09-varying_rhythm.py
@@ -29,7 +29,7 @@ K % str() >> Print()    # Returns the tonic key (I)
 motif = N * 6 << Foreach(quarter, eight, eight, dotted_quarter, eight, whole) >> S
 motif << Foreach(-3, 1, 2, 3, 2, -3)**Degree()
 
-mutated_motif = TranslocateRhythm(motif) * 29 % Sequence()
+mutated_motif = TranslocateRhythm(motif) * 29 % Sequence() >> Stack()
 
 # Where the Variation pitch is generated (Foreach does iteration contrary to Subject)
 varying_rhythm = motif >> mutated_motif
--- src/operand_mutation.py
+++ src/operand_mutation.py
@@ -190,7 +190,7 @@ class Translocation(Mutation):
         super().__init__()
         self._chaos: ch.Chaos           = ch.SinX()
         self._filter: od.Filter         = od.Filter(of.All())
-        self._parameters: od.Parameters = od.Parameters(ot.Position(), ra.NoteValue())
+        self._parameters: od.Parameters = od.Parameters(ot.Position())
         if len(parameters) > 0:
             self << parameters
 
@@ -256,7 +256,7 @@ class Translocation(Mutation):
                 jumbled_result.shuffle(self._chaos) # a single shuffle
                 for single_parameter in self._parameters._data: # A tuple of parameters
                     source_result << of.Foreach(jumbled_result)**of.Get(single_parameter)
-                self._result % od.DataSource() >> od.Stack()    # It only changes the Duration, so it requires a Stack NOT a Link!
+                # self._result % od.DataSource() >> od.Stack()    # It only changes the Duration, so it requires a Stack NOT a Link!
                 if actual_iteration % muted_iterations == 0:
                     self.perform(number)
                 self._index += 1    # keeps track of each iteration
