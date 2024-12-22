53ab4ccff8433a6e5d7f8b7cdca82a03be072cbb
Rui Monteiro <ruiseixas@yahoo.com>
2024-12-22 18:19:35 +0000

    Diminution example

---
+++ Books/MusicComposition/IdiotsGuide/Part_04/Chapter_13/11-diminution.py
@@ -0,0 +1,36 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+import sys
+import os
+src_path = os.path.join(os.path.dirname(__file__), '../../../../..', 'src')
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from JsonMidiCreator import *
+
+rest_play = (R, P)
+staff << "#" << 120
+K % str() >> Print()    # Returns the tonic key (I)
+
+# Original Motif to work on its pitches
+motif = N * 6 << Foreach(quarter, eight, eight, dotted_quarter, eight, whole) >> S
+motif << Foreach(-3, 1, 2, 3, 2, -3)**Degree()
+
+diminished_motif = motif.copy() << Get(Duration())**Divide(2) >> Stack()
+
+# Where the Variation pitch is generated (Foreach does iteration contrary to Subject)
+varying_rhythm = motif >> diminished_motif
+varying_rhythm >> rest_play
