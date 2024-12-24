1d027845d27bf832574c51ba017b295efe768792
Rui Monteiro <ruiseixas@yahoo.com>
2024-12-24 20:28:07 +0000

    Tested Sequence __add__ method

--- pytests/test_operand_container.py
+++ pytests/test_operand_container.py
@@ -80,4 +80,4 @@ def test_add_elements():
     four_notes: Sequence = Note() * 4
 
     # assert two_notes * 2 == four_notes
-    # assert two_notes + two_notes == four_notes
+    assert two_notes + two_notes >> Stack() == four_notes
