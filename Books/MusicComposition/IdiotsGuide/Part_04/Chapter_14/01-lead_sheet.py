c7d8ebba0b35a79733f47ddd0c6ef6f31f5d02aa
Rui Monteiro <ruiseixas@yahoo.com>
2025-01-04 07:24:28 +0000

    Book lead sheet example

--- Books/MusicComposition/IdiotsGuide/Part_04/Chapter_14/01-lead_sheet.py
+++ Books/MusicComposition/IdiotsGuide/Part_04/Chapter_14/01-lead_sheet.py
@@ -25,9 +25,21 @@ rest_play = (R, P)
 staff << KeySignature(-1)   # Sets the default Key Note configuration
 
 
-four_eights: Sequence = Note(eight) * 4
-four_eights -= Foreach()
-four_eights >> Play()
+four_eights: Sequence = Note(eight) * 4 + 3 # Increases 3 degrees from I to IV, from F to Bb
+four_eights -= Iterate()    # Decreases degrees from IV to I
+# four_eights >> Play()
 
+first_half_1: Sequence = Note() + 2 * Note(eight) >> Stack() << Foreach(3, 2, 3)
+# first_half_1 >> Play()
 
+first_half_2: Sequence = first_half_1 * 1 << Nth(1)**2
+# first_half_2 >> Play()
 
+melody: Sequence = first_half_1 >> four_eights >> first_half_2 >> Note(1/2, 2) * 1 >> first_half_2 - 1 >> four_eights - 1 >> Note(1/1) << Track("melody")
+# melody * 4 >> Play()
+
+chords: Sequence = Chord(1) + Chord(2) + Chord(6, 1/2) + Chord(3, 1/2) + Chord(6) >> Stack() << Track("chords") << Octave(3)
+# chords * 4 >> Play()
+
+lead_sheet: Song = melody + chords
+lead_sheet >> Play()
