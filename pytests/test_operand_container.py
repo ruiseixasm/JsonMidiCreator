0594bbf1db8be81e217a4d44030a2f2dcd61e64b
Rui Monteiro <ruiseixas@yahoo.com>
2025-01-06 00:03:34 +0000

    Improved Sequence length method

--- pytests/test_operand_container.py
+++ pytests/test_operand_container.py
@@ -316,16 +316,16 @@ def test_sequence_operations():
     assert straight_length % Name() == "Length"
     
     straight_serialization: dict = straight_length.getSerialization()
-    straight_serialization % Data("float") >> Print()   # 1/8 + 1/4 + 1/4 * 3/2 + 1/8 * 3/2 = 15/16 NoteValue = 4 * 15/16 = 15/4 = 3.75 Beats
-    assert straight_serialization % Data("float") == 3.75
+    straight_serialization % Data("float") >> Print()   # 3.75 Beats => 1 Measure = 4.0 Beats
+    assert straight_serialization % Data("float") == 4.0
 
     reversed_length: Length = reversed_sequence % Length()
     reversed_length % Name() >> Print()
     assert reversed_length % Name() == "Length"
     
     reversed_serialization: dict = reversed_length.getSerialization()
-    reversed_serialization % Data("float") >> Print()   # 1/8 * 3/2 + 1/4 * 3/2 + 1/4 + 1/8 = 15/16 NoteValue = 4 * 15/16 = 15/4 = 3.75 Beats
-    assert reversed_serialization % Data("float") == 3.75
+    reversed_serialization % Data("float") >> Print()   # 3.75 Beats => 1 Measure = 4.0 Beats
+    assert reversed_serialization % Data("float") == 4.0
 
     assert straight_sequence != reversed_sequence
     assert straight_sequence.copy().reverse()[0] == reversed_sequence[0] + Beats(0.25)
--- src/operand_container.py
+++ src/operand_container.py
@@ -440,7 +440,10 @@ class Sequence(Container):  # Just a container of Elements
 
 
     def length(self) -> ra.Length:
-        return ra.Length(self.finish() - self.start())
+        finish_position: ra.Position = self.finish()
+        sequence_length: ra.Length = ra.Length( finish_position )
+        sequence_length << ra.Measures(sequence_length % ou.Measure())  # Rounded up Duration to next Measure
+        return sequence_length
 
     def duration(self) -> ra.Duration:
         return (self.finish() - self.start()).getDuration()
