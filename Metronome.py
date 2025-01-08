c41dcb81816ffe46326878735b8fc66ea5589b03
Rui Monteiro <ruiseixas@yahoo.com>
2025-01-08 04:29:24 +0000

    Added Metronome file

---
+++ Metronome.py
@@ -0,0 +1,38 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+import sys
+import os
+src_path = os.path.join(os.path.dirname(__file__), '', 'src')
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from JsonMidiCreator import *
+
+rest_play = (R, P)
+staff << Tempo(120)     # Sets the metronome tempo
+staff << Measures(4)    # Sets the length in Measures
+
+measures_long: int = staff // Measures() // int()
+
+measure_bell: Sequence = Nt(DrumKit(34)) * 1 * measures_long
+measure_bell >> Play()
+beat_tick: Sequence = (Nt(DrumKit(35)) * 3 + Beat(1)) * measures_long << Velocity(70)
+beat_tick >> Play()
+
+metronome: Sequence = measure_bell + beat_tick
+metronome >> Play()
+
+
--- YouTube/Syncopation/03-chords_syncopation.py
+++ YouTube/Syncopation/03-chords_syncopation.py
@@ -26,6 +26,8 @@ staff << KeySignature(+1, Minor())  # Sets the default Key Signature configurati
 
 # https://youtu.be/7rhZAXjhPzI?si=7qEpDmaWQ80skir2
 
+
+
 hi_hat: Seq = Nt(Dur(staff % Quant()), DrumKit("Hi-Hat")) * 16 << NotEqual(Step(0))**Velocity(70)
 hi_hat *= 4     # 4 measures long
 # hi_hat << Disable()
@@ -34,11 +36,12 @@ hi_hat *= 4     # 4 measures long
 kick: Seq = Nt(Dur(staff % Quant()), DrumKit("Drum"), Stackable(False)) * 4 + Iterate(Beats(1))
 kick *= 4       # 4 measures long
 kick << Vel(80) # less pronounced kick
+kick << Disable()
 # kick >> Play()
 
 clap: Seq = Nt(Dur(staff % Quant()), DrumKit("Clap"), Stackable(False)) * 2 + Iterate(Beats(1)) + Beats(1)
 clap *= 4       # 4 measures long
-# clap << Disable()
+clap << Disable()
 # clap >> Play()
 
 no_syncopation: Seq = hi_hat + kick + clap
@@ -49,7 +52,7 @@ base_line: Seq = Nt(dotted_eight) * Measures(4) # Tonic note E in E minor (see K
 base_line << Octave(1)  # Sets it as a Base line, lower Octave
 base_line << Velocity(70)   # Reduces the velocity to make it less prominent
 base_line[0] % str() >> Print() # Prints the real key being played
-# base_line << Disable()
+base_line << Disable()
 
 syncopation_1: Seq = no_syncopation + (base_line + Step(1) << 1/16)
 # syncopation_1 >> Play()
@@ -61,13 +64,16 @@ chords -= NotEqual(Measure(0))**Octave(1)
 chords -= Octave(1)
 chords *= 4
 chords << Velocity(80)  # Chords tend to be loud, so they need to be softened
-# chords << Disable()
+chords << Disable()
 # chords >> Play()
 
-syncopation_2: Seq = syncopation_1 * 4 + chords # x4 because chords are 4x longer than the original syncopation
-syncopation_2 >> Play()
+# syncopation_1 >> Play()
 print("Delay for 0.5 seconds")
 time.sleep(0.5)
+syncopation_1 * 4 >> Play()
+
+syncopation_2: Seq = syncopation_1 * 4 + chords # x4 because chords are 4x longer than the original syncopation
+# syncopation_2 >> Play()
 
 
 
