a9ff9c1d40b9c88c9fb3fceeba099074f8bb53f9
Rui Monteiro <ruiseixas@yahoo.com>
2025-01-22 14:42:46 +0000

    Adjusted YouTube examples to use Chord([]) as configured to use the KeySignature

--- YouTube/Syncopation/04.1-chords_syncopation_adjustments_faster.py
+++ YouTube/Syncopation/04.1-chords_syncopation_adjustments_faster.py
@@ -65,7 +65,7 @@ syncopation_1: Clip = no_syncopation + base_line
 # print("Delay for 0.5 seconds")
 # time.sleep(0.5)
 
-chords: Clip = Chord() * 4 << Foreach(1, 5, 6, 4)    # Sets Chords Degree
+chords: Clip = Chord([]) * 4 << Foreach(1, 5, 6, 4)    # Sets Chords Degree
 chords -= NotEqual(Measure(0))**Octave(1)
 chords -= Octave(1)
 chords *= 4
