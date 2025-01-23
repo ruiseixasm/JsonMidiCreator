729e4d447031bac8158ee456a008d0bbc4efd961
Rui Monteiro <ruiseixas@yahoo.com>
2025-01-23 22:50:27 +0000

    Added new gray book example

---
+++ Books/MakingMusic/DennisDeSantis/2-ProblemsProgressing/150-PolyrhythmicLoops/01-juxtaposition.py
@@ -0,0 +1,36 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+import sys
+import os
+src_path = os.path.join(os.path.dirname(__file__), '../../../../..', 'src')
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from JsonMidiCreator import *
+
+rest_play = (R, P)
+defaults << Tempo(120)
+
+basic_loop: Clip = Note(1/16) * 4 << Foreach(2, 4, 6, 4) << Octave(3) << Velocity(65)
+out_sync_loop: Clip = Note(1/16) + Rest() >> Stack()
+
+# Multiply by elements instead of Measures (by float instead of by int)
+out_sync_loop *= 4.0
+basic_loop *= 5.0
+
+juxtaposition: Clip = basic_loop + out_sync_loop
+juxtaposition * 8.0 >> P
+
