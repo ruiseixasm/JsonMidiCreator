da73b15306feb3522b209c2b06aa65da68d48210
Rui Monteiro <ruiseixas@yahoo.com>
2025-01-24 08:33:07 +0000

    Improved print output in the last example

--- Books/MakingMusic/DennisDeSantis/2-ProblemsProgressing/160-Rhythm/03-strong_contrast.py
+++ Books/MakingMusic/DennisDeSantis/2-ProblemsProgressing/160-Rhythm/03-strong_contrast.py
@@ -29,6 +29,6 @@ tresillo *= tresillo / Filter(Nth(1, 2)) << Foreach(B2, B3)
 clap: Clip = Note(DrumKit("Clap"), 1/16) * 4 << Iterate(2)**Add(1)**Beats()
 
 strong_contrast: Clip = tresillo + clap
-strong_contrast % Length() % float() >> Print()
+strong_contrast % Length() % Steps() % float() >> Print()
 
 strong_contrast * 8 >> P
