e5b8d91998f3cc7b967ce2465a26ffdb53c1be3e
Rui Monteiro <ruiseixas@yahoo.com>
2025-01-24 05:50:08 +0000

    Improved Grey book example

--- Books/MakingMusic/DennisDeSantis/2-ProblemsProgressing/160-Rhythm/01-3_3_2_rhythm.py
+++ Books/MakingMusic/DennisDeSantis/2-ProblemsProgressing/160-Rhythm/01-3_3_2_rhythm.py
@@ -30,3 +30,8 @@ hi_hat: Clip = Note(DrumKit("Hi-Hat"), 1/16) * 4 << Iterate(2)**Steps() << TimeS
 
 (tresillo >> snare >> hi_hat) * 16 >> P
 
+snare << TimeSignature(4, 4) << Beats(2) # Base 0 NOT base 1
+hi_hat |= Step(2)
+
+(tresillo >> hi_hat) * 16 >> snare * 8 >> P
+
