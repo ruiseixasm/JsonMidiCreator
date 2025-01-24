f0a3125a26fb0dbb2cc9c2f5fb282e2649a166dc
Rui Monteiro <ruiseixas@yahoo.com>
2025-01-24 05:00:45 +0000

    Added new Grey book example

---
+++ Books/MakingMusic/DennisDeSantis/2-ProblemsProgressing/160-Rhythm/01-3_3_2_rhythm.py
@@ -0,0 +1,32 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+import sys
+import os
+src_path = os.path.join(os.path.dirname(__file__), '../../../../..', 'src')
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from JsonMidiCreator import *
+
+rest_play = (R, P)
+defaults << Tempo(110)
+
+tresillo: Clip = Note(DrumKit("Drum"), 1/16) * 3 << Iterate(3)**Steps() << TimeSignature(2, 4)
+snare: Clip = Note(DrumKit("Snare"), 1/16, Step(4)) * 1 << TimeSignature(2, 4)
+hi_hat: Clip = Note(DrumKit("Hi-Hat"), 1/16) * 4 << Iterate(2)**Steps() << TimeSignature(2, 4)
+
+tresillo * 4 >> snare * 4 >> hi_hat * 4 >> P
+
