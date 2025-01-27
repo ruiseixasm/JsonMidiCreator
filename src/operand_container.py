6e3171449d49b7c560db792676c793cabf11fbd5
Rui Monteiro <ruiseixas@yahoo.com>
2025-01-27 09:01:39 +0000

    Concluded __imul__ of Clip class improvements

--- src/operand_container.py
+++ src/operand_container.py
@@ -777,10 +777,10 @@ class Clip(Container):  # Just a container of Elements
     def __imul__(self, operand: any) -> Self:
         match operand:
             case Clip():
-                left_end_position: ra.Position = self.finish()
-                self_length: ra.Length = self % ra.Length()
-                if left_end_position > self_length:
-                    left_end_position = self_length.convertToPosition()
+                if self._length_beats < 0:
+                    left_end_position: ra.Position = self.finish()
+                else:
+                    left_end_position: ra.Position = (self % ra.Length()).convertToPosition()
                 right_start_position: ra.Position = operand.start()
                 length_shift: ra.Length = ra.Length(left_end_position - right_start_position).roundMeasures()
                 # Convert Length to Position
