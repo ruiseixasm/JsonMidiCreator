52d82fb56a4883cf393a205173c3c85236b9df1a
Rui Monteiro <ruiseixas@yahoo.com>
2025-01-31 10:40:04 +0000

    Added variable signature_tonic_key to the Pitch method get_key_float

--- src/operand_generic.py
+++ src/operand_generic.py
@@ -283,6 +283,7 @@ class Pitch(Generic):
                     key_int += 1
 
             key_scale: list[int] = self._major_scale     # Major scale
+            signature_tonic_key: int = self._staff_reference._key_signature % ou.Key() % int() % 12
             root_key: int = key_int
 
             semitone_transpose: int = 0
