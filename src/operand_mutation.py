a1a196bcf3be80a0783fc108f82cc2f1edb4f303
Rui Monteiro <ruiseixas@yahoo.com>
2025-02-27 13:30:58 +0000

    Added the Mutation subclass Division

--- src/operand_mutation.py
+++ src/operand_mutation.py
@@ -541,12 +541,12 @@ class Multiplication(Operation):
             clip <<= multiplied_clips // list()
         return clip
 
-# class Division(Operation):
-#     def mutate(self, clip: o.T) -> o.T:
-#         if isinstance(clip, oc.Clip):
-#             shuffled_clips: list[oc.Clip] = self.shuffle_list(self._clips)
-#             multiplied_clips: oc.Clip = oc.Clip()
-#             for single_clip in shuffled_clips:
-#                 multiplied_clips /= single_clip
-#             clip <<= multiplied_clips // list()
-#         return clip
+class Division(Operation):
+    def mutate(self, clip: o.T) -> o.T:
+        if isinstance(clip, oc.Clip):
+            shuffled_clips: list[oc.Clip] = self.shuffle_list(self._clips)
+            multiplied_clips: oc.Clip = oc.Clip()
+            for single_clip in shuffled_clips:
+                multiplied_clips /= single_clip
+            clip <<= multiplied_clips // list()
+        return clip
