494d449ea7ff52254132ed428af5bcbbf90709da
Rui Monteiro <ruiseixas@yahoo.com>
2025-03-09 05:40:09 +0000

    Simplified example

--- YouTube/Melody/01-melody_from_chords.py
+++ YouTube/Melody/01-melody_from_chords.py
@@ -32,8 +32,7 @@ chords = Chord() * 4 << Foreach("I", "IV", "V", "I")**Degree()
 # chords >> Play()
 
 chord_notes_i = Chord(Degree("I")) % Clip()
-# chord_notes_i >> Play()
 chord_notes_iv = Chord(Degree("IV")) % Clip()
-# chord_notes_iv >> Play()
-chord_notes_i * chord_notes_iv >> Play()
+chord_notes_v = Chord(Degree("V")) % Clip()
+chord_notes_i * chord_notes_iv * chord_notes_v * chord_notes_i * 4 >> Play()
 
