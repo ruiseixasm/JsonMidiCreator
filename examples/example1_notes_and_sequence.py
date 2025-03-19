668c1571a12f74d745794808415ff4c9c15b0946
Rui Monteiro <ruiseixas@yahoo.com>
2025-03-19 05:55:03 +0000

    Adjusted testing examples

--- examples/example1_notes_and_sequence.py
+++ examples/example1_notes_and_sequence.py
@@ -60,7 +60,7 @@ first_sequence = Rest(2/1, Channel(10)) * first_sequence
 # second_sequence >> Play()
 
 # Creations, aggregation of both Sequences in a Track element and respective Play
-all_elements = Part(first_sequence) + second_sequence >> Save("json/_Save_1.6_all_elements.json") # HAS TO BECOME A SONG !!!
+all_elements = first_sequence >> second_sequence >> Save("json/_Save_1.6_all_elements.json") # HAS TO BECOME A SONG !!!
 Length( Beats(2) ) >> first_note
 all_elements += first_note + single_clock
 all_elements >> Play() >> Export("json/_Export_1.2_all_elements.json")  # IT'S GONNA BE A SONG SAVE !!
