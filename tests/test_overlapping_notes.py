47c953d8be2dc4c770d19d62f01a2b341c70ef53
Rui Monteiro <ruiseixas@yahoo.com>
2025-03-23 19:19:11 +0000

    Added overlapping example

---
+++ tests/test_overlapping_notes.py
@@ -0,0 +1,34 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+import sys
+import os
+src_path = os.path.join(os.path.dirname(__file__), '..', 'src')
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from JsonMidiCreator import *
+
+defaults << Tempo(60)
+
+beat_tick = Note(Channel(10), Pitch(33.0)) * 4 << Duration(1/16)
+
+first_note = Note(1/2)
+second_note = Note(1/8) + Step(1)
+
+simultaneous_notes = beat_tick + first_note + second_note
+
+simultaneous_notes * 8 >> MidiExport("Midi/overlapping_notes.mid") >> P
+
