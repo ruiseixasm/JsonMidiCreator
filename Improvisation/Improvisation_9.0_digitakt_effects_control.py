c006a7100dc3007d4ad816dddc4b6aa97bf758f2
Rui Monteiro <ruiseixas@yahoo.com>
2025-03-24 04:25:31 +0000

    Concluded the Midi Digitakt table

--- Improvisation/Improvisation_9.0_digitakt_effects_control.py
+++ Improvisation/Improvisation_9.0_digitakt_effects_control.py
@@ -151,11 +151,240 @@ midi_cc: dict[str,
                 "Routing": {
                     "MSB": 88, "LSB": -1   # Blank, no LSB, 7 bits
                 }
-            }
-
-
+            },
+            "AMP": {
+                "Attack": {
+                    "MSB": 78, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Hold": {
+                    "MSB": 79, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Decay": {
+                    "MSB": 80, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Overdrive": {
+                    "MSB": 81, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Delay": {
+                    "MSB": 82, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Reverb": {
+                    "MSB": 83, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Pan": {
+                    "MSB": 10, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Volume": {
+                    "MSB": 7, "LSB": -1   # Blank, no LSB, 7 bits
+                }
+            },
+            "LFO 1": {
+                "Speed": {
+                    "MSB": 102, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Multiplier": {
+                    "MSB": 103, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Fade": {
+                    "MSB": 104, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Destination": {
+                    "MSB": 105, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Waveform": {
+                    "MSB": 106, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Phase": {
+                    "MSB": 107, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Mode": {
+                    "MSB": 108, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Depth": {
+                    "MSB": 109, "LSB": 61   # Non blank, with LSB, 14 bits
+                }
+            },
+            "LFO 2": {
+                "Speed": {
+                    "MSB": 112, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Multiplier": {
+                    "MSB": 113, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Fade": {
+                    "MSB": 114, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Destination": {
+                    "MSB": 115, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Waveform": {
+                    "MSB": 116, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Phase": {
+                    "MSB": 117, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Mode": {
+                    "MSB": 118, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Depth": {
+                    "MSB": 119, "LSB": 63   # Non blank, with LSB, 14 bits
+                }
+            },
+            "MIDI": {
+                "Val1": {
+                    "MSB": 70, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Val2": {
+                    "MSB": 71, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Val3": {
+                    "MSB": 72, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Val4": {
+                    "MSB": 73, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Val5": {
+                    "MSB": 74, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Val6": {
+                    "MSB": 75, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Val7": {
+                    "MSB": 76, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Val8": {
+                    "MSB": 77, "LSB": -1   # Blank, no LSB, 7 bits
+                }
+            },
 
             # PER PATTERN PARAMETERS (CHANNEL 9)
+            "DELAY": {
+                "Time": {
+                    "MSB": 85, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Pingpong": {
+                    "MSB": 86, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Width": {
+                    "MSB": 87, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Feedback": {
+                    "MSB": 88, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Highpass": {
+                    "MSB": 89, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Lowpass": {
+                    "MSB": 90, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Reverb": {
+                    "MSB": 91, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Mix": {
+                    "MSB": 92, "LSB": -1   # Blank, no LSB, 7 bits
+                }
+            },
+            "REVERB": {
+                "Predelay": {
+                    "MSB": 24, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Decay": {
+                    "MSB": 25, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Frequency": {
+                    "MSB": 26, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Gain": {
+                    "MSB": 27, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Highpass": {
+                    "MSB": 28, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Lowpass": {
+                    "MSB": 29, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Reverb": {
+                    "MSB": 30, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Mix": {
+                    "MSB": 31, "LSB": -1   # Blank, no LSB, 7 bits
+                }
+            },
+            "COMPRESSOR": {
+                "Threshold": {
+                    "MSB": 111, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Attack": {
+                    "MSB": 112, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Release": {
+                    "MSB": 113, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Gain": {
+                    "MSB": 114, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Volume": {
+                    "MSB": 119, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Ratio": {
+                    "MSB": 115, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Source": {
+                    "MSB": 116, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Filter": {
+                    "MSB": 117, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Mix": {
+                    "MSB": 118, "LSB": -1   # Blank, no LSB, 7 bits
+                }
+            },
+            "MIXER": {
+                "Level L": {
+                    "MSB": 102, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Pan L": {
+                    "MSB": 103, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Level R": {
+                    "MSB": 104, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Pan R": {
+                    "MSB": 105, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Delay L": {
+                    "MSB": 106, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Delay R": {
+                    "MSB": 107, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Reverb L": {
+                    "MSB": 108, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Reverb R": {
+                    "MSB": 109, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "Stereo": {
+                    "MSB": 84, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "LR Level": {
+                    "MSB": 102, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "LR Balance": {
+                    "MSB": 103, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "LR Delay": {
+                    "MSB": 106, "LSB": -1   # Blank, no LSB, 7 bits
+                },
+                "LR Reverb": {
+                    "MSB": 108, "LSB": -1   # Blank, no LSB, 7 bits
+                }
+            },
+            "PATTERN": {
+                "Mute": {
+                    "MSB": 110, "LSB": -1   # Blank, no LSB, 7 bits
+                }
+            }
         }
 
 
