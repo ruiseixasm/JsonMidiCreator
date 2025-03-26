4d1e2c706b4f417169922182734f635a962f9ead
Rui Monteiro <ruiseixas@yahoo.com>
2025-03-26 11:04:49 +0000

    Blofeld Example

--- Improvisation/Blofeld/Improvisation_10.2_blofeld_mixer.py
+++ Improvisation/Blofeld/Improvisation_10.2_blofeld_mixer.py
@@ -30,10 +30,11 @@ long_note_c = Note(1/1) * 4
 
 print("1st LOOP")
 
-level_cc = ControlChange(Blofeld.midi_cc["MIXER COMMON"]["Pan"]) * 16 << Iterate(0, -7)
-level_cc *= ControlChange(Blofeld.midi_cc["MIXER COMMON"]["Pan"]) * 16 << Iterate(-7*16, 7)
-level_cc *= ControlChange(Blofeld.midi_cc["MIXER COMMON"]["Pan"]) * 16 << Iterate(0, 7)
-level_cc *= ControlChange(Blofeld.midi_cc["MIXER COMMON"]["Pan"]) * 16 << Iterate(7*16, -7)
+measure_cc = ControlChange(Blofeld.midi_cc["MIXER COMMON"]["Pan"]) * 16
+level_cc = Clip( measure_cc << Iterate(0, -7) ) 
+level_cc *= measure_cc << Iterate(-7*16, 7)
+level_cc *= measure_cc << Iterate(0, 7)
+level_cc *= measure_cc << Iterate(7*16, -7)
 (level_cc + long_note_c) * 8 >> P
 
 print("2nd LOOP")
