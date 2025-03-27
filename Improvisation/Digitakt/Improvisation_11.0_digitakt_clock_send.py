c206dc162f6148a4941bd5f5624c76bd48a697db
Rui Monteiro <ruiseixas@yahoo.com>
2025-03-27 17:31:41 +0000

    Created Digitakt Clock testing

---
+++ Improvisation/Digitakt/Improvisation_11.0_digitakt_clock_send.py
@@ -0,0 +1,41 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+from jsonmidicreator_import import *    # This ensures src is added & JsonMidiCreator is imported
+
+defaults % Devices() % list() >> Print()
+defaults += Digitakt.device
+defaults % Devices() % list() >> Print()
+
+# Send Clock signal to the Digitakt
+defaults % ClockedDevices() % list() >> Print()
+defaults << ClockedDevices(Digitakt.device)
+defaults % ClockedDevices() % list() >> Print()
+
+
+long_rest = Rest(2/1)
+long_element = Element(2/1)
+
+long_rest % Length() % float() >> Print()
+long_element % Length() % float() >> Print()
+
+long_rest >> P
+time.sleep(0.5)
+long_element >> P
+
+
+defaults -= Digitakt.device
+defaults % Devices() % list() >> Print()
+
