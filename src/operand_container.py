65f6182542c2a8b531c4ea7561b98abee3d3f4ba
Rui Monteiro <ruiseixas@yahoo.com>
2025-03-31 17:13:37 +0100

    Staff has no clock!

--- src/operand_container.py
+++ src/operand_container.py
@@ -1025,11 +1025,10 @@ class Clip(Composition):  # Just a container of Elements
                     case om.Mutation():     operand._data.mutate(self)
                     case _:                 super().__lshift__(operand)
 
-            case og.Staff() | ou.KeySignature() | og.TimeSignature() | ra.StaffParameter() | ClockedDevices() | \
-                    ou.Accidentals() | ou.Major() | ou.Minor():
-                self._staff << operand
             case ou.MidiTrack() | ou.TrackNumber() | od.TrackName() | Devices() | od.Device():
                 self._midi_track << operand
+            case og.Staff() | ou.KeySignature() | og.TimeSignature() | ra.StaffParameter() | ou.Accidentals() | ou.Major() | ou.Minor():
+                self._staff << operand  # Staff has no clock!
             # Use Frame objects to bypass this parameter into elements (Setting Position)
             case od.Serialization():
                 self.loadSerialization( operand.getSerialization() )
