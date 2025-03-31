af09ef72c941fec0a1f91e0979d45bb1d31e0a9c
Rui Monteiro <ruiseixas@yahoo.com>
2025-03-31 09:51:23 +0100

    Created file basic composition

---
+++ Improvisation/Muli/Improvisation_1.0_basic_composition.py
@@ -0,0 +1,31 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+from jsonmidicreator_import import *    # This ensures src is added & JsonMidiCreator is imported
+
+
+
+# Devices to sync
+defaults << ClockedDevices("Digitakt", "Blofeld", "Virtual")
+# defaults += Device("Digitakt")
+
+# One Measure clips
+pattern_change_1 = Clip(ProgramChange(1, Digitakt.auto_channel))
+pattern_change_2 = Clip(ProgramChange(2, Digitakt.auto_channel))
+
+# Cycle patterns change
+pattern_change_1 * pattern_change_2 * 4 >> Play(1)
+
+
