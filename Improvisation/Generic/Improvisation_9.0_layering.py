1a11bc6edae9e3f3f71bd91ad0d30e7661505117
Rui Monteiro <ruiseixas@yahoo.com>
2025-04-02 15:38:35 +0100

    Layering chaos

--- Improvisation/Generic/Improvisation_9.0_layering.py
+++ Improvisation/Generic/Improvisation_9.0_layering.py
@@ -18,22 +18,39 @@ from jsonmidicreator_import import *    # This ensures src is added & JsonMidiCr
 
 
 defaults += Device("Blofeld")
-start_program = Blofeld.program_change(4, "A") >> P
+Blofeld.program_change(4, "A") >> P
 # Devices to sync that also guarantee the total playing up to the end of Measures
 # Note that Rest has no impact im prolonging the playing time without the global Clock on
 defaults << ClockedDevices("Blofeld")
 
-two_notes = Note() / 2 << Iterate(step=2)**Beats()
-possibilities = Loop(1, 2, 3, 4, 5, 6, 7)**Degree()
+# Configuration input list
+iterations: list[int] = [-1, 2, 8]
+
+clip_1 = Note(Channel(1)) * 4
+clip_2 = Note(Channel(2)) * 4 + Steps(2)
+clip_3 = Note(Channel(3)) * 4 + Steps(3)
+part: Part = Part()
+chaos: Chaos = Chaos() * 1233
+layer: int = 0
 
 
-for degree in range(1, 8):  # 7 degrees in total
-    degree >> Pr
-    two_notes >> Nth(2) << degree
-    try_clip = two_notes * 2 * Rest()
-    try_clip >> P
-    
+clip_1 << Nth(2)**Input(chaos * iterations[layer])**Pick(2, 4, 5)
+if iterations[layer] >= 0:
+    part += clip_1
+layer += 1
 
+clip_2 << Nth(3)**Input(chaos * iterations[layer])**Pick(2, 4, 5)
+if iterations[layer] >= 0:
+    part += clip_2
+layer += 1
 
-reset_program = Blofeld.program_change(1, "B") > P
+clip_3 << Nth(4)**Input(chaos * iterations[layer])**Pick(2, 4, 5)
+if iterations[layer] >= 0:
+    part += clip_3
+layer += 1
+
+part >> Pv
+
+defaults << ClockedDevices()
+Blofeld.program_change(1, "A") + AllNotesOff() >> P
 
