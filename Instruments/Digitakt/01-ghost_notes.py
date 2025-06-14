b4dbb8e34f92ef9b854d64cd02033ce0b1df527a
Rui Monteiro <ruiseixas@yahoo.com>
2025-06-14 17:30:46 +0100

    Cleaned up many of the Use class usage

--- Instruments/Digitakt/01-ghost_notes.py
+++ Instruments/Digitakt/01-ghost_notes.py
@@ -38,7 +38,7 @@ drum = Note(DrumKit("Drum"), 1/16) * 3 << Iterate()**Beats()
 drum += Nth(2)**Steps(3)
 
 ghost_notes = Note(DrumKit("Snare"), 1/16) * 2 + Step(1) << Velocity(30)
-ghost_notes << Use(Length(0.5))
+ghost_notes << Length(0.5)
 ghost_notes *= 2
 
 if INSTRUMENT:
--- pytests/test_operand_container.py
+++ pytests/test_operand_container.py
@@ -542,7 +542,7 @@ def test_mul_clip():
     six_notes = 6 * Note()
     print(f"Length: {six_notes % Length() % float()}")
     assert six_notes % Length() == 1.5  # Measures
-    six_notes << Use(Length(1.0))
+    six_notes << Length(six_notes, 1.0)
     print(f"Length: {six_notes % Length() % float()}")
     assert six_notes % Length() == 1.0  # Measures
 
