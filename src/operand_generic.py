b367ef291e70ecd5d1e31dd9d4d10533fd0b2c70
Rui Monteiro <ruiseixas@yahoo.com>
2025-07-03 10:49:10 +0100

    Methods transpose_scale and modulate_tonic moved to the Scale class

--- src/operand_generic.py
+++ src/operand_generic.py
@@ -410,11 +410,11 @@ class Pitch(Generic):
                         scale_tonic: int = self_staff % ou.Tonic() % int()
 
                     if self._transpose:
-                        transposition: int = Staff.transpose_scale(self._shifting, modulated_scale)
+                        transposition: int = Scale.transpose_scale(self._shifting, modulated_scale)
                         root_key += transposition # Jumps by semitones (chromatic tones)
                     else:
                         tonic_offset: int = root_key - scale_tonic
-                        root_key += Staff.modulate_tonic(tonic_offset, self._shifting, modulated_scale)
+                        root_key += Scale.modulate_tonic(tonic_offset, self._shifting, modulated_scale)
 
                 return float( 12 * (self._octave + 1) + self.get_key_with_accidentals(root_key) )
             
@@ -1052,6 +1052,37 @@ class Scale(Generic):
         super().__init__(*parameters)
 
 
+    @staticmethod
+    def transpose_scale(steps: int = 4, scale: list[int] = [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1]) -> int:
+        # The given scale shall always have a size of 12
+        scale_transposition: int = 0
+        if len(scale) == 12 and sum(1 for key in scale if key != 0) > 0:
+            while steps > 0:
+                scale_transposition += 1
+                if scale[scale_transposition % 12]:
+                    steps -= 1
+            while steps < 0:
+                scale_transposition -= 1
+                if scale[scale_transposition % 12]:
+                    steps += 1
+        return scale_transposition
+
+    @staticmethod
+    def modulate_tonic(tonic_offset: int = 0, degrees_0: int = 4, scale: list[int] = [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1]) -> int:
+        # The given scale shall always have a size of 12
+        tonic_modulation: int = 0
+        if len(scale) == 12 and sum(1 for key in scale if key == 1) > 0:
+            while degrees_0 > 0:
+                tonic_modulation += 1
+                if scale[ (tonic_offset + tonic_modulation) % 12 ] == 1:  # Scale key
+                    degrees_0 -= 1
+            while degrees_0 < 0:
+                tonic_modulation -= 1
+                if scale[ (tonic_offset + tonic_modulation) % 12 ] == 1:  # Scale key
+                    degrees_0 += 1
+        return tonic_modulation
+
+
     def __mod__(self, operand: o.T) -> o.T:
         """
         The % symbol is used to extract a Parameter, a Scale has many extraction modes
@@ -1442,37 +1473,6 @@ class Staff(Generic):
         return self << parameters
     
     
-    @staticmethod
-    def transpose_scale(steps: int = 4, scale: list[int] = [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1]) -> int:
-        # The given scale shall always have a size of 12
-        scale_transposition: int = 0
-        if len(scale) == 12 and sum(1 for key in scale if key != 0) > 0:
-            while steps > 0:
-                scale_transposition += 1
-                if scale[scale_transposition % 12]:
-                    steps -= 1
-            while steps < 0:
-                scale_transposition -= 1
-                if scale[scale_transposition % 12]:
-                    steps += 1
-        return scale_transposition
-
-    @staticmethod
-    def modulate_tonic(tonic_offset: int = 0, degrees_0: int = 4, scale: list[int] = [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1]) -> int:
-        # The given scale shall always have a size of 12
-        tonic_modulation: int = 0
-        if len(scale) == 12 and sum(1 for key in scale if key == 1) > 0:
-            while degrees_0 > 0:
-                tonic_modulation += 1
-                if scale[ (tonic_offset + tonic_modulation) % 12 ] == 1:  # Scale key
-                    degrees_0 -= 1
-            while degrees_0 < 0:
-                tonic_modulation -= 1
-                if scale[ (tonic_offset + tonic_modulation) % 12 ] == 1:  # Scale key
-                    degrees_0 += 1
-        return tonic_modulation
-
-
     def convert_time_to_measures(self, minutes: int = 0, seconds: int = 0) -> int:
         actual_bps: Fraction = self._tempo / 60 # Beats Per Second
         time_seconds: int = 60 * minutes + seconds
