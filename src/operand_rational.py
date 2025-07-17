bca591b278c0f060a73742dafa1680e9e2355128
Rui Monteiro <ruiseixas@yahoo.com>
2025-07-17 22:08:44 +0100

    Corrected Measurement Division and Multiplication

--- src/operand_rational.py
+++ src/operand_rational.py
@@ -888,8 +888,8 @@ class Measurement(Convertible):
                 self_measures: Measures = self % Measures()
                 operand_measures: Measures = operand % Measures(self._time_signature_reference)
                 self << self_measures * operand_measures
-            case int() | float():
-                self *= Measures(operand)  # Default variable is Measures
+            # case int() | float():
+            #     self *= Measures(operand)  # Default variable is Measures
             case _:
                 super().__imul__(operand)
         return self
@@ -903,9 +903,9 @@ class Measurement(Convertible):
                 operand_measures: Measures = operand % Measures(self._time_signature_reference)
                 if operand_measures != Measures(0):
                     self << self_measures / operand_measures
-            case int() | float():
-                if operand != 0:
-                    self /= Measures(operand)  # Default variable is Measures
+            # case int() | float():
+            #     if operand != 0:
+            #         self /= Measures(operand)  # Default variable is Measures
             case _:
                 super().__itruediv__(operand)
         return self
@@ -1110,10 +1110,10 @@ class Duration(Measurement):
                 self_notevalue: NoteValue = self % NoteValue()
                 operand_notevalue: NoteValue = operand % NoteValue()
                 self << self_notevalue * operand_notevalue
-            case int():
-                self *= Steps(operand)
-            case float():
-                self *= NoteValue(operand)
+            # case int():
+            #     self *= Steps(operand)
+            # case float():
+            #     self *= NoteValue(operand)
             case _:
                 super().__imul__(operand)
         return self
@@ -1126,10 +1126,10 @@ class Duration(Measurement):
                 operand_notevalue: NoteValue = operand % NoteValue()
                 if operand_notevalue != NoteValue(0):
                     self << self_notevalue / operand_notevalue
-            case int():
-                self /= Steps(operand)
-            case float():
-                self /= NoteValue(operand)
+            # case int():
+            #     self /= Steps(operand)
+            # case float():
+            #     self /= NoteValue(operand)
             case _:
                 super().__itruediv__(operand)
         return self
