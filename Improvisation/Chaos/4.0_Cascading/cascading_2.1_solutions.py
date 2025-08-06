92106a7308c07d52d4abac36ff5e40f76964a385
Rui Monteiro <ruiseixas@yahoo.com>
2025-08-06 22:55:21 +0100

    Clean code

--- Improvisation/Chaos/4.0_Cascading/cascading_2.1_solutions.py
+++ Improvisation/Chaos/4.0_Cascading/cascading_2.1_solutions.py
@@ -21,8 +21,8 @@ def composition(clip: Clip) -> Composition:
     return snare + clip
 
 seed_notes = Note() / 8
-rhythm_solution = RS_Clip(seed_notes)
+clip_solution = RS_Clip(seed_notes)
 
-phrase_notes = rhythm_solution.rhythm_fast_quantized(7).tonality_conjunct_but_slacked(3).solution()
+phrase_notes = clip_solution.rhythm_fast_quantized(7).tonality_conjunct_but_slacked(3).solution()
 phrase_notes >> Plot()
 
