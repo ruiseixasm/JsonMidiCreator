4f65703a0a24223e3211d138c3b62929b2a92c34
Rui Monteiro <ruiseixas@yahoo.com>
2025-08-06 17:54:21 +0100

    Created some resource functions

--- src/resource_functions.py
+++ src/resource_functions.py
@@ -32,18 +32,40 @@ import operand_generic as og
 import operand_element as oe
 import operand_frame as of
 import operand_container as oc
+import operand_tamer as ot
 import operand_chaos as ch
 
 
-
-class F_Rhythm:
+class F_Function:
+    def get_iterate_function(self) -> Callable[['oc.Composition'], 'oc.Composition']:
+        return lambda composition: self.iterate(composition)
     
-    def four_on_the_floor(*parameters, measures: int = 4) -> oc.Clip:
-        pattern: oc.Clip = oc.Clip(og.TimeSignature(4, 4), oe.Note(1/16), parameters)
-        pattern *= measures       
-        return pattern
+    def iterate(self, composition: 'oc.Composition') -> 'oc.Composition':
+        return composition
 
 
-class F_Pitch:
+class F_Rhythm(F_Function):
+    pass
+
+class F_Rhythm_Fast_Quantized(F_Rhythm):
+    def __init__(self,
+                 durations: list[float] = [1/8 * 3/2, 1/8, 1/16 * 3/2, 1/16, 1/32 * 3/2, 1/32],
+                 choices: list[int] = [2, 4, 4, 2, 1, 1, 3],
+                 chaos: ch.Chaos = ch.SinX(340)):
+        # floats are implicitly Note Values for `Element` operands
+        self.durations: list[float] = durations
+        self.choices: list[int] = choices
+        self.chaos: ch.Chaos = chaos
+
+    def iterate(self, clip: 'oc.Clip') -> 'oc.Clip':
+        if isinstance(clip, oc.Clip):
+            self.chaos._tamer.reset()   # Tamer needs to be reset
+            picked_durations = o.list_choose(self.durations, self.chaos % self.choices)
+            clip << of.Foreach(*picked_durations)**ra.NoteValue()
+            return clip.stack().quantize().mul([0]).link().mul(4)
+        return clip
+
+
+class F_Pitch(F_Function):
     pass
 
