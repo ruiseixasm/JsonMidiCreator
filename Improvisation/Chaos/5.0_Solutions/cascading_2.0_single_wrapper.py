522c01ce0e1aea8168658895526bc5823c48e957
Rui Monteiro <ruiseixas@yahoo.com>
2025-08-28 16:18:55 +0100

    single_wrapper solution example

---
+++ Improvisation/Chaos/5.0_Solutions/cascading_2.0_single_wrapper.py
@@ -0,0 +1,25 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+from jsonmidicreator_import import *    # This ensures src is added & JsonMidiCreator is imported
+
+
+quarter_notes = Note() / 4
+def composition(clip: Clip) -> Composition:
+    return clip
+
+rhythm_solution = RS_Clip(quarter_notes * 4, c_button=composition)
+moved_beats = rhythm_solution.single_wrapper(-10, by_channel=True).solution()
+
