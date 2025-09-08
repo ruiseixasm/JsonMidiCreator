d5a498122df30fc2ec3084e2a8d785cb0cb77a49
Rui Monteiro <ruiseixas@yahoo.com>
2025-09-08 15:53:36 +0100

    Simplified example

--- Improvisation/Chaos/5.0_Solutions/solutions_9.0_operate_parameter.py
+++ Improvisation/Chaos/5.0_Solutions/solutions_9.0_operate_parameter.py
@@ -35,8 +35,9 @@ phrase_notes = clip_solution.mask(Measure(3)).operate_parameter(
         parameter=Degree()
     ).mask(Measure(3), Beat(2)).operate_parameter(
         9,
-        chaos=SinX(Wrap(int())**Repeat()**Increase(1)**Modulo(7)),
-        parameter=Semitone()
+        chaos=Cycle(),
+        parameter=Semitone(1),  # Has to set 1 to trigger iterations!
+        operator=lambda element, result: element.sub(result)
     ).solution()
 
 
