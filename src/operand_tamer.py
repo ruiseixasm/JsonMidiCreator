14afd8d2633c73e1164bc16f2170821dfb0ffe46
Rui Monteiro <ruiseixas@yahoo.com>
2025-09-19 03:59:47 +0100

    Tamer classes replaced  ^=  with rxor

--- src/operand_tamer.py
+++ src/operand_tamer.py
@@ -166,7 +166,7 @@ class Parallel(Tamer):
         return self
         
     def __lshift__(self, operand: any) -> Self:
-        operand ^= self    # Processes the Frame operand if any exists
+        self.__rxor__(operand)    # Processes the Frame operand if any exists
         match operand:
             case Tamer():
                 super().__lshift__(operand)
@@ -246,7 +246,7 @@ class Validator(Tamer):
         return self
         
     def __lshift__(self, operand: any) -> Self:
-        operand ^= self    # Processes the Frame operand if any exists
+        self.__rxor__(operand)    # Processes the Frame operand if any exists
         match operand:
             case Validator():
                 super().__lshift__(operand)
@@ -307,7 +307,7 @@ class Check(Validator):
     # CHAINABLE OPERATIONS
 
     def __lshift__(self, operand: any) -> Self:
-        operand ^= self    # Processes the Frame operand if any exists
+        self.__rxor__(operand)    # Processes the Frame operand if any exists
         match operand:
             case self.__class__():
                 super().__lshift__(operand)
@@ -369,7 +369,7 @@ class Boundary(Validator):
         return self
         
     def __lshift__(self, operand: any) -> Self:
-        operand ^= self    # Processes the Frame operand if any exists
+        self.__rxor__(operand)    # Processes the Frame operand if any exists
         match operand:
             case self.__class__():
                 super().__lshift__(operand)
@@ -470,7 +470,7 @@ class Prior(Validator):
         return self
         
     def __lshift__(self, operand: any) -> Self:
-        operand ^= self    # Processes the Frame operand if any exists
+        self.__rxor__(operand)    # Processes the Frame operand if any exists
         match operand:
             case self.__class__():
                 super().__lshift__(operand)
@@ -607,7 +607,7 @@ class Motion(Validator):
         return self
         
     def __lshift__(self, operand: any) -> Self:
-        operand ^= self    # Processes the Frame operand if any exists
+        self.__rxor__(operand)    # Processes the Frame operand if any exists
         match operand:
             case Motion():
                 super().__lshift__(operand)
@@ -701,7 +701,7 @@ class Pattern(Motion):
         return self
         
     def __lshift__(self, operand: any) -> Self:
-        operand ^= self    # Processes the Frame operand if any exists
+        self.__rxor__(operand)    # Processes the Frame operand if any exists
         match operand:
             case Pattern():
                 super().__lshift__(operand)
@@ -927,7 +927,7 @@ class Manipulator(Tamer):
         return self
         
     def __lshift__(self, operand: any) -> Self:
-        operand ^= self    # Processes the Frame operand if any exists
+        self.__rxor__(operand)    # Processes the Frame operand if any exists
         match operand:
             case self.__class__():
                 super().__lshift__(operand)
