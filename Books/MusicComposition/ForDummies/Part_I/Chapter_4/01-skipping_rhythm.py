511b8f55ef6b2677079b78740c415f54f49b2151
Rui Monteiro <ruiseixas@yahoo.com>
2025-09-22 17:36:17 +0100

    Added book example

---
+++ Books/MusicComposition/ForDummies/Part_I/Chapter_4/01-skipping_rhythm.py
@@ -0,0 +1,28 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+import sys
+import os
+src_path = os.path.join(os.path.dirname(__file__), '../../../../..', 'src')
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from JsonMidiCreator import *
+
+left = Note(Channel(10), Pitch(43), Velocity(80)) / [Dotted(1/8), 1/16]
+right = left.copy(Pitch(37))
+
+left / right / 4 * 2 << Name("Skipping Rhythm") >> Plot()
+
