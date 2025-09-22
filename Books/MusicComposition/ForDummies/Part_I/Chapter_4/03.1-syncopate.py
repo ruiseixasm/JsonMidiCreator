48a3978ac2e9b36525a775d25593e7606ca3c4d9
Rui Monteiro <ruiseixas@yahoo.com>
2025-09-22 23:06:08 +0100

    Added examples

---
+++ Books/MusicComposition/ForDummies/Part_I/Chapter_4/03.1-syncopate.py
@@ -0,0 +1,28 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+import sys
+import os
+src_path = os.path.join(os.path.dirname(__file__), '../../../../..', 'src')
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from JsonMidiCreator import *
+
+syncopation = Note(Octave(5)) / list_wrap([3, 1, 3, 1, 3, 1, 8, 3, 1, 4], Steps()) + Beat(1)
+syncopation += Foreach(0, 0, -3, -3, -2, -2, -3, -1, -1, 0)**Degree()
+
+syncopation * 2 << Name("Syncopation") >> Plot()
+
