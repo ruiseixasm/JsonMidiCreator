1238cee2295d16538c56edaf9ea84f1e030f4d8c
Rui Monteiro <ruiseixas@yahoo.com>
2025-09-26 23:12:48 +0100

    Book example

--- Books/MusicComposition/IdiotsGuide/Part_04/Chapter_14/17-move_smoothly.py
+++ Books/MusicComposition/IdiotsGuide/Part_04/Chapter_14/17-move_smoothly.py
@@ -27,24 +27,22 @@ ProgramChange(Channel(11), Program("Violin")) \
     + ProgramChange(Channel(13), Program("Viola")) \
     + ProgramChange(Channel(14), Program("Cello")) >> Play()   # Sets the instruments
 
-violin_1 = Note(Channel(11), 1/1) / 4 << Name("Violin 1") << Nth(2, 3)**(Minor(), Degree(-1)) << Nth(3, 4)**Foreach("D", "G")**TonicKey()
-violin_1 << Degree(5)   # Static Voicing
+violin_1 = Note(Channel(11), 1/1, Octave(5)) / 4 << Name("Violin 1")
+violin_1 << Foreach("1", "3", "6", "1")
 
-violin_2 = Note(Channel(12), Velocity(80), 1/1) / 4 << Name("Violin 2") << Nth(2, 3)**(Minor(), Degree(-1)) << Nth(3, 4)**Foreach("D", "G")**TonicKey()
-violin_2 << Degree(3)   # Static Voicing
+violin_2 = Note(Channel(12), Velocity(80), 1/1) / 4 << Name("Violin 2")
+violin_2 << Foreach("V", "vii", "iii", "vi")
 
-viola = Note(Channel(13), 1/1) / 4 << Name("Viola") << Nth(2, 3)**(Minor(), Degree(-1)) << Nth(3, 4)**Foreach("D", "G")**TonicKey()
-viola << Degree(1)      # Static Voicing
+viola = Note(Channel(13), 1/1) / 4 << Name("Viola")
+viola << Foreach("3", "7", "1", "4")
 
-cello = Note(Channel(14), Octave(3), 1/1, Velocity(70)) / 4 << Name("Cello") << Nth(2, 3)**(Minor(), Degree(-1)) << Nth(3, 4)**Foreach("D", "G")**TonicKey()
-cello << Degree(1)      # Static Voicing
+cello = Note(Channel(14), Octave(3), 1/1, Velocity(70)) / 4 << Name("Cello")
+cello << Foreach("I", "iii", "vi", "iv")
 
-# violin_1 + violin_2 + viola + cello >> Plot(block=False)
-violin_1 >>= Smooth()
-violin_2 >>= Smooth()
-viola >>= Smooth()
-cello >>= Smooth()
+violin_2 >>= Smooth(True)
+viola >>= Smooth(True)
+cello >>= Smooth(True)
 
 static_voicing: Part = Part([violin_1, violin_2, viola, cello])
-static_voicing * 4 << Name("Static Voicing") >> Plot(block=False)
+static_voicing * 4 << Name("Skip Wise Motion") >> Plot()
 
