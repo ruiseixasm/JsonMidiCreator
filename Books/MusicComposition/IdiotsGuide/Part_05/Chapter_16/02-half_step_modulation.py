edaa2dcf2333b8253477c6a0827082999021b774
Rui Monteiro <ruiseixas@yahoo.com>
2025-10-01 12:43:36 +0100

    Half-Step example

---
+++ Books/MusicComposition/IdiotsGuide/Part_05/Chapter_16/02-half_step_modulation.py
@@ -0,0 +1,31 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+import sys
+import os
+src_path = os.path.join(os.path.dirname(__file__), '../../../../..', 'src')
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from JsonMidiCreator import *
+
+
+settings << Folder("Books/MusicComposition/IdiotsGuide/Part_05/Chapter_16/")
+
+major_C = Note(1/2) / 3 << Last()**(1/1) << Title("Modulation Up Half Step") << Foreach("3", "4", "5")
+major_Db = major_C + Semitone(1)
+
+major_C * major_Db * 4 >> Plot()
+
