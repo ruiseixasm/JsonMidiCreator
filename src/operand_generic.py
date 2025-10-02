660a34eaa1591653fdf9724ce64ef5c41d29a87c
Rui Monteiro <ruiseixas@yahoo.com>
2025-10-02 22:58:26 +0100

    Optimized code

--- src/operand_generic.py
+++ src/operand_generic.py
@@ -880,12 +880,10 @@ class Pitch(Generic):
                         root_pitch: int = self.chromatic_root_int() + self.octave_transposition()
                         root_octave_0: int = root_pitch // 12   # root_octave may be different from self._octave_0
                         if root_octave_0 != expected_octave_0:  # For mismatch Octaves
-                            self += ou.Octave( expected_octave_0 - root_octave_0 )
+                            self._octave_0 += expected_octave_0 - root_octave_0
                     case ou.TargetKey():
-                        expected_octave_0: int = operand._data._unit // 12
                         self << operand._data  # Sets the TargetKey on the actual Octave
-                        if self._octave_0 != expected_octave_0: # For mismatch Octaves
-                            self += ou.Octave( expected_octave_0 - self._octave_0 )
+                        self._octave_0 = operand._data._unit // 12
 
                     case ou.Key():
                         self << od.Pipe( ou.RootKey(operand._data._unit) )
