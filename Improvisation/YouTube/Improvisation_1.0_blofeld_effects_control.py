21340f4ba24d18db3f03a66fcc3e5d59e7004242
Rui Monteiro <ruiseixas@yahoo.com>
2025-10-05 19:50:01 +0100

    Improved example

--- Improvisation/YouTube/Improvisation_1.0_blofeld_effects_control.py
+++ Improvisation/YouTube/Improvisation_1.0_blofeld_effects_control.py
@@ -30,8 +30,8 @@ settings << Folder("Improvisation/YouTube/") << Tempo(140)
 # melody_6_00 = Note().read()
 # melody_6_00 >> Plot()
 
-melody_6_00 = Load("track_1_clip_0_0_eda8_save.json") + Step(10) >> Quantize()
-melody_6_00 << Foreach("4", "5", "5", "4", "3", "1", "2", "6", "1", "5", "4", "3", "4", "6", "2")
+melody_6_00 = Load("track_1_clip_0_0_eda8_save.json") + Step(10) << Channel(11) >> Quantize()
+melody_6_00 << Foreach(4, 5, 5, 4, 3, 1, 2, 6, 1, 5, 4, 3, 4, 6, 2)**"" # Wrapped as str() in **""
 melody_6_00 << Foreach(3, 3, 4, 4, 4, 4, 4, 3, 4, 3, 3, 3, 3, 3)**Octave()
 melody_6_00 >> Plot(block=False)
 settings << Quantization(1/2)   # After works, before doesn't
