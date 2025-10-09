21264b5bbb7c8e32ef007758925ac3a904527dde
Rui Monteiro <ruiseixas@yahoo.com>
2025-10-09 14:01:10 +0100

    Improved example

--- Books/EasyToPlayKeyboardTunes/10-o_little_town_of_bethlehem.py
+++ Books/EasyToPlayKeyboardTunes/10-o_little_town_of_bethlehem.py
@@ -26,35 +26,39 @@ from JsonMidiCreator import *
 settings << Folder("Books/EasyToPlayKeyboardTunes/")
 
 
-ProgramChange("Percussive organ", Channel(1)) + ProgramChange("Bass", Channel(2)) >> Play()
+ProgramChange("Trumpet", Channel(1)) + ProgramChange("Mandolin", Channel(2)) >> Play()
 
 settings << Tempo(140) << TimeSignature(4, 4) << KeySignature('#') << Quantization(1/2)
 
 
 pickup_bar = \
-    N4(Beat(3)) / 1
+    N4(Beat(3))**1
 main_phrase = \
     N4("G")**3 / N4("A") / N8("B") / N8("A") / N8("B") / N8("C") / N4("D") / N4("B")
-variant_phrase = \
-    N4() / N8()**2 / N4()**2 / ND2() / N4() << Foreach("C", "B", "G", "A", "A", "G", "D")
+variant_phrase_ = \
+    N4() / N8()**2 / N4()**2 / ND2() << Foreach("C", "B", "G", "A", "A", "G")
 central_period = \
-    ND4("D") / N8("E")**5 / N8()**["G", "A", "B", "C"] / N4("D")**2 \
-        << Match(0, "E")**Previous(Pipe(Degree()))**Subtract(1)**Pipe() \
-        << Match(1, Beat(3))**Octave(4)
+    ND4("D") / N8("E")**5 / N8()**["G", "A", "B", "C"] / N4("D")**2 / \
+    N4()**["G", "B", "A", "G"] / N2("D")**2 \
+        << Match(0, "E")**Previous(Pipe(Degree()))**Subtract(1)**Pipe()
 
-melody = pickup_bar * main_phrase * variant_phrase * central_period
-melody << Title("O Little Town of Bethlehem") << Velocity(85)
+melody = \
+    pickup_bar * \
+    main_phrase * variant_phrase_ / N4("D") * main_phrase * variant_phrase_ / N8("G") / N8("B") * \
+    central_period * main_phrase * variant_phrase_ >> Smooth() << First()**("D", Octave(4))
+melody << Title("Melody") << Velocity(85) >> Plot(block=False)
 
 
 
 chords = \
-    C2("D", 3, Beat(2)) / C_3("G") / C2()**["C", "D"] / \
-    C_4("G") / C1("D") / C_2("G") / C2()**["C", "D"] / C1("G")
+    R_1(1)**2 / R2() / \
+    C2("D") / C_3("G") / C2("C") / C2("D") / \
+    C_4("G") / C_1("D") / C_2("G") / C2("C") / C2("D") / C_1("G")
 
-chords << Channel(2) << Octave(3) << Velocity(70) << Gate(.99)
+chords << Channel(2) << Octave(3) << Velocity(90) << Gate(.99)
 chords >>= Smooth(4)
 
 
-melody / 2 + chords.copy(Disable()) / 2 >> Plot(composition=chords / 2)
+melody / 2 + chords.copy(Disable()) / 2 >> Plot(composition=chords / 2, title="O Little Town of Bethlehem")
 
 
