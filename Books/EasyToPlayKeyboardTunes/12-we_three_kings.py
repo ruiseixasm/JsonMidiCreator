7c8f5cd01a1a529a76834b355862098c5a27667a
Rui Monteiro <ruiseixas@yahoo.com>
2025-10-14 18:03:55 +0100

    Clean code

--- Books/EasyToPlayKeyboardTunes/12-we_three_kings.py
+++ Books/EasyToPlayKeyboardTunes/12-we_three_kings.py
@@ -79,7 +79,7 @@ melody *= "n,b2,C,o5    n,b1,C,o5   n,b2,D,o5   n,b1,E,o5"
 chords *= "c,b3,F   c,b3,G"
 melody *= "n,b2,F5    n,b1,E5   n,b2,D5   n,b1,E5"
 # Measure 14
-chords *= "c,b6,C"
+chords *= "c_1,C"
 melody *= "n4,C5   n8,C5  n4,C5  n8,G"
 # Measure 15
 chords *= "c,b3,F   c,b3,C"
