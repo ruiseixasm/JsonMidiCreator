3f9884d2058e9d31deeb3a2e35a2fe160aa0e9fb
Rui Monteiro <ruiseixas@yahoo.com>
2025-10-22 03:17:13 +0100

    Added Rhythms improvisations

---
+++ Improvisation/Rhythms/01-rhythm_three_one.py
@@ -0,0 +1,24 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+from jsonmidicreator_import import *    # This ensures src is added & JsonMidiCreator is imported
+
+
+settings << Folder("Improvisation/Rhythms/")
+settings << Tempo(120) << TimeSignature(3, 4)
+
+
+rhythm = Clip("1/8,G 1/8,G# 1/8,A :1/4,G")
+rhythm >> Plot()
