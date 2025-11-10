f616594755e2142dd76521100ec6aef6b8222935
Rui Monteiro <ruiseixas@yahoo.com>
2025-11-10 03:12:01 +0000

    Clean code

--- src/operand_container.py
+++ src/operand_container.py
@@ -2606,10 +2606,9 @@ class Clip(Composition):  # Just a container of Elements
     def _set_time_signature(self, time_signature: 'og.TimeSignature') -> Self:
         beats_per_measure_ratio: Fraction \
             = time_signature % ra.BeatsPerMeasure() % Fraction() / self._time_signature._top
-        if beats_per_measure_ratio != 1:    # Avoids recursion among many items
-            for element in self._items:
-                element._position_beats *= beats_per_measure_ratio
-                element._duration_beats *= beats_per_measure_ratio
+        for element in self._items:
+            element._position_beats *= beats_per_measure_ratio
+            element._duration_beats *= beats_per_measure_ratio
         self._time_signature << time_signature
         return self
 
