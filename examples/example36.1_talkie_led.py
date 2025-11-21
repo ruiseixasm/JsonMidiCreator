4496fdbc37cf537195607291d5945e878447c90a
Rui Monteiro <ruiseixas@yahoo.com>
2025-11-21 18:43:50 +0000

    Messaging working

---
+++ examples/example36.1_talkie_led.py
@@ -0,0 +1,33 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+import sys
+import os
+import time
+src_path = os.path.join(os.path.dirname(__file__), '..', 'src')
+
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from JsonMidiCreator import *
+
+settings << Folder("examples/") << Tempo(140)
+
+turn_on = TalkieRun(To("ESP32"), "on", 1/16)
+turn_off = TalkieRun(To("ESP32"), "off", 1/32)
+lighting = turn_on / turn_off / 50
+lighting >> Export("talkie_esp32_light.json") >> Play(verbose=True)
+
+
