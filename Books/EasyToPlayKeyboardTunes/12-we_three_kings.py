ec06e68648f8c6d891337ba880e9e0601bc59d79
Rui Monteiro <ruiseixas@yahoo.com>
2026-05-24 05:14:43 +0100

    Commented out the old lines style

--- Books/EasyToPlayKeyboardTunes/12-we_three_kings.py
+++ Books/EasyToPlayKeyboardTunes/12-we_three_kings.py
@@ -31,68 +31,68 @@ ProgramChange("Electric grand piano", Channel(1)) + ProgramChange("Synth voice",
 settings << Tempo(240) << TimeSignature(6, 8) << KeySignature('') << Quantization(1/2)
 
 
-# / has precedence over +
+# # / has precedence over +
 
-chords = Clip()
-melody = Clip()
+# chords = Clip()
+# melody = Clip()
 
-# Measure 0
-chords *= "c;m1;A"
-melody *= "n;1/4;E;o5  n;1/8;D;o5  n;1/4;C;o5  n;1/8;A"
-# Measure 1
-chords *= "c;d1/4;G             c;b9;A"
-melody *= "n;1/8;B  n;1/8;C;o5  n;1/8;B  n;d1/4;A"
-# Measure 2
-chords *= ""
-melody *= "n;1/4;E;o5  n;1/8;D;o5  n;1/4;C;o5  n;1/8;A" # Repeated from MEasure 0
-# Measure 3
-chords *= "c;d1/4;G          c;d2/4;A"
-melody *= "n;1/8;B  n;1/8;C;o5  n;1/8;B  n;d1/4;A"  # Repeated from MEasure 1
-# Measure 4
-chords *= "c;p0.5;d1/4;G"
-melody *= "n;b2;C;o5  n;b1;C;o5   n;b2;D;o5  n;b1;D;o5"
-# Measure 5
-chords *= "c;b3;C   c;b3;G"
-melody *= "n;b2;E;o5   n;b1;E;o5   n;b1;G;o5  n;b1;F;o5  n;b1;E;o5"
-# Measure 6
-chords *= "c;b3;D   c;b3;E"
-melody *= "n;b1;D;o5   n;b1;E;o5   n;b1;D;o5  n;b2;C;o5  n;b1;B"
-# Measure 7
-chords *= "c;b3;A   c;b3;G"
-melody *= "n;b3;A   n;b2;B  n;b1;D;o5"
-# Measure 8
-chords *= "c;b6;A"
-melody *= "n;b2;C;o5   n;b1;C;o5  n;b2;C;o5  n;b1;G"
-# Measure 9
-chords *= "c;b3;F   c;b3;C"
-melody *= "n;b2;C;o5   n;b1;A  n;b3;C;o5"
-# Measure 10
-chords *= "c;b6;C"
-melody *= "n;b2;C;o5   n;b1;C;o5  n;b2;C;o5     n;b1;G"
-# Measure 11
-chords *= "c;b3;F   c;b3;C"
-melody *= "n;b2;C;o5    n;b1;A  n;b3;C;o5"
-# Measure 12
-chords *= "c;b3;A   c;b3;G"
-melody *= "n;b2;C;o5    n;b1;C;o5   n;b2;D;o5   n;b1;E;o5"
-# Measure 13
-chords *= "c;b3;F   c;b3;G"
-melody *= "n;b2;F5    n;b1;E5   n;b2;D5   n;b1;E5"
-# Measure 14
-chords *= "c_1;C"
-melody *= "n4;C5   n8;C5  n4;C5  n8;G"
-# Measure 15
-chords *= "c4d;F    c4d;C"
-melody *= "b2;C5    b1;A    b3;C5"
+# # Measure 0
+# chords *= "c;m1;A"
+# melody *= "n;1/4;E;o5  n;1/8;D;o5  n;1/4;C;o5  n;1/8;A"
+# # Measure 1
+# chords *= "c;d1/4;G             c;b9;A"
+# melody *= "n;1/8;B  n;1/8;C;o5  n;1/8;B  n;d1/4;A"
+# # Measure 2
+# chords *= ""
+# melody *= "n;1/4;E;o5  n;1/8;D;o5  n;1/4;C;o5  n;1/8;A" # Repeated from MEasure 0
+# # Measure 3
+# chords *= "c;d1/4;G          c;d2/4;A"
+# melody *= "n;1/8;B  n;1/8;C;o5  n;1/8;B  n;d1/4;A"  # Repeated from MEasure 1
+# # Measure 4
+# chords *= "c;p0.5;d1/4;G"
+# melody *= "n;b2;C;o5  n;b1;C;o5   n;b2;D;o5  n;b1;D;o5"
+# # Measure 5
+# chords *= "c;b3;C   c;b3;G"
+# melody *= "n;b2;E;o5   n;b1;E;o5   n;b1;G;o5  n;b1;F;o5  n;b1;E;o5"
+# # Measure 6
+# chords *= "c;b3;D   c;b3;E"
+# melody *= "n;b1;D;o5   n;b1;E;o5   n;b1;D;o5  n;b2;C;o5  n;b1;B"
+# # Measure 7
+# chords *= "c;b3;A   c;b3;G"
+# melody *= "n;b3;A   n;b2;B  n;b1;D;o5"
+# # Measure 8
+# chords *= "c;b6;A"
+# melody *= "n;b2;C;o5   n;b1;C;o5  n;b2;C;o5  n;b1;G"
+# # Measure 9
+# chords *= "c;b3;F   c;b3;C"
+# melody *= "n;b2;C;o5   n;b1;A  n;b3;C;o5"
+# # Measure 10
+# chords *= "c;b6;C"
+# melody *= "n;b2;C;o5   n;b1;C;o5  n;b2;C;o5     n;b1;G"
+# # Measure 11
+# chords *= "c;b3;F   c;b3;C"
+# melody *= "n;b2;C;o5    n;b1;A  n;b3;C;o5"
+# # Measure 12
+# chords *= "c;b3;A   c;b3;G"
+# melody *= "n;b2;C;o5    n;b1;C;o5   n;b2;D;o5   n;b1;E;o5"
+# # Measure 13
+# chords *= "c;b3;F   c;b3;G"
+# melody *= "n;b2;F5    n;b1;E5   n;b2;D5   n;b1;E5"
+# # Measure 14
+# chords *= "c_1;C"
+# melody *= "n4;C5   n8;C5  n4;C5  n8;G"
+# # Measure 15
+# chords *= "c4d;F    c4d;C"
+# melody *= "b2;C5    b1;A    b3;C5"
 
 
-chords *= 2
-melody *= 2
+# chords *= 2
+# melody *= 2
 
-melody << Title("Melody") << Velocity(85) >> Plot(block=False)
-chords << Channel(2) << Octave(3) << Velocity(60) << Gate(.99)
-chords >>= Smooth(4)
-melody + chords.copy(Disable()) >> Plot(block=False, composition=chords, title="We Three Kings")
+# melody << Title("Melody") << Velocity(85) >> Plot(block=False)
+# chords << Channel(2) << Octave(3) << Velocity(60) << Gate(.99)
+# chords >>= Smooth(4)
+# melody + chords.copy(Disable()) >> Plot(block=False, composition=chords, title="We Three Kings")
 
 
 chords = Line()
