1e4afb74c031ea58d3f8c2ec00ed4fed4ee822dd
Rui Monteiro <ruiseixas@yahoo.com>
2026-05-29 22:41:01 +0100

    Cleaned is_enharmonic method

--- src/operand_unit.py
+++ src/operand_unit.py
@@ -328,11 +328,24 @@ class KeySignature(PitchParameter):       # Sharps (+) and Flats (-)
         return og.Scale._scales[scale_mode]
 
 
-    def is_enharmonic(self, tonic_key: int, key: int) -> bool:
-        # if self._major_scale[tonic_key % 12] != 1:
-        #     return True
+    def is_enharmonic(self, key: int) -> bool:
         self_key_signature: list[int] = self._key_signatures[(self._unit + 7) % 15]
         return self_key_signature[key % 12] != 0
+
+
+    def _get_accidental(self, tonic_key: int = None) -> int:
+        accidental: int = 0
+        if tonic_key is None:
+            tonic_key = self.get_tonic_key()
+        else:
+            tonic_key %= 12
+        # It happens only for 7 Flats (-7) (Cb)
+        if self.is_enharmonic(tonic_key):
+            if self._unit < 0:
+                accidental = -1
+            else:
+                accidental = +1
+        return accidental
 
 
     def _get_key_line(self, tonic_key: int = None) -> int:
@@ -344,7 +357,7 @@ class KeySignature(PitchParameter):       # Sharps (+) and Flats (-)
         if self._unit < 0:
             key_line = 1    # To get b instead of #
         # It happens only for 7 Flats (-7) (Cb)
-        if self.is_enharmonic(tonic_key, tonic_key):
+        if self.is_enharmonic(tonic_key):
             key_line += 2    # All Sharps/Flats
         return key_line
 
@@ -368,7 +381,7 @@ class KeySignature(PitchParameter):       # Sharps (+) and Flats (-)
                 if self._unit < 0:
                     key_line = 1    # To get b instead of #
                 # It happens only for 7 Flats (-7) (Cb)
-                if self.is_enharmonic(tonic_key, tonic_key):
+                if self.is_enharmonic(tonic_key):
                     key_line += 2    # All Sharps/Flats
                 return Key(tonic_key, float(key_line))
             
