1f95e0fb20304a787eb6acb81acf341100370e84
Rui Monteiro <ruiseixas@yahoo.com>
2026-05-30 11:39:23 +0100

    Safer code

--- src/operand_generic.py
+++ src/operand_generic.py
@@ -922,7 +922,10 @@ class Pitch(Generic):
                     self << ou.Natural()
                 else:
                     self << ou.Degree(string) # Safe, doesn't change the octave
-                    self << (self % ou.Key() << string) # Preserves the Key Signature type
+                    target_key: int = self._get_target_key()
+                    new_key_operand = ou.Key(target_key, string)
+                    if new_key_operand._unit != target_key:
+                        self << new_key_operand
                     if len(operand) > 1:    # Single value shouldn't set the Octave
                         self << (self % ou.Octave() << string)
                     self << Scale(od.Pipe(self._scale), operand)
