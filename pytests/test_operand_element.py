4361aa1f6b02efae07bd7e70166751b9a575f61f
Rui Monteiro <ruiseixas@yahoo.com>
2026-06-12 22:58:04 +0100

    4 to go

--- pytests/test_operand_element.py
+++ pytests/test_operand_element.py
@@ -592,35 +592,35 @@ def test_note_pitch():
 def test_chord_element():
 
     triad: Chord = Chord()
-    assert Note(triad) % str() == "C"
+    assert Note(triad) % Key() % str() == "C"
     triad_notes: list[Note] = triad.get_component_elements()
     #                                +4   +3
     expected_keys: list[str] = ["C", "E", "G"]
     for key in range(3):
-        triad_notes[key] % TargetKey() % str() >> Print()
-        assert triad_notes[key] % TargetKey() % str() == expected_keys[key]
+        triad_notes[key] % Key() % str() >> Print()
+        assert triad_notes[key] % Key() % str() == expected_keys[key]
 
     # WITH DEGREE
     print("------")
     triad << Degree("ii") << Scale([])  # Uses the Staff signature (Major Scale)
-    assert Note(triad) % TargetKey() % str() == "D"
+    assert Note(triad) % Key() % str() == "D"
     triad_notes = triad.get_component_elements()
     #                      +3   +4
     expected_keys = ["D", "F", "A"]
     for key in range(3):
-        triad_notes[key] % TargetKey() % str() >> Print()
-        assert triad_notes[key] % TargetKey() % str() == expected_keys[key]
+        triad_notes[key] % Key() % str() >> Print()
+        assert triad_notes[key] % Key() % str() == expected_keys[key]
 
     # WITH ROOT NOTE
     print("------")
     triad << Degree("I") << "D" << Scale("Major")
-    assert Note(triad) % TargetKey() % str() == "D"
+    assert Note(triad) % Key() % str() == "D"
     triad_notes = triad.get_component_elements()
     #                      +4   +3
     expected_keys = ["D", "F#", "A"]
     for key in range(3):
-        triad_notes[key] % TargetKey() % str() >> Print()
-        assert triad_notes[key] % TargetKey() % str() == expected_keys[key]
+        triad_notes[key] % Key() % str() >> Print()
+        assert triad_notes[key] % Key() % str() == expected_keys[key]
 
 # test_chord_element()
 
