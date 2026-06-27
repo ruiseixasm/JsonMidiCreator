5c8b90326d77087b9b7b6073abbf71b40d732a9e
Rui Monteiro <ruiseixas@yahoo.com>
2026-06-27 19:50:44 +0100

    Added method filter_all_but_one_same_duration

---
+++ src/resource_filters.py
@@ -0,0 +1,55 @@
+'''
+JsonMidiCreator - Json Midi Creator is intended to be used
+in conjugation with the Json Midi Player to Play composed Elements
+Original Copyright (c) 2024 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonMidiCreator
+https://github.com/ruiseixasm/JsonMidiPlayer
+'''
+# Example using typing.Union (compatible with Python < 3.10)
+from typing import Union, TypeVar, TYPE_CHECKING, Type, Callable, List, Tuple, Optional, Any, Generic
+from typing import Self
+
+from fractions import Fraction
+import json
+
+# Json Midi Creator Libraries
+import creator as c
+import operand as o
+
+import operand_unit as ou
+import operand_rational as ra
+import operand_data as od
+import operand_label as ol
+import operand_generic as og
+import operand_element as oe
+import operand_frame as of
+import operand_container as oc
+import operand_chaos as ch
+
+
+
+class RF_Iterations:
+
+    def filter_all_but_one_same_duration(clip: 'oc.Clip'):
+        durations: dict[Fraction, int] = {}
+        for element in clip:
+            duration_beats: Fraction = element._duration_beats
+            if duration_beats in durations:
+                durations[duration_beats] += 1
+            else:
+                durations[duration_beats] = 1
+        if len(durations) == 2:
+            for amount in durations.values():
+                if amount == 1:
+                    return True
+        return False
+        
+
