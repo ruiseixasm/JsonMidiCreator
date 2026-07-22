fda81cac8d44086fd5891b8382dc24dfe4c2c35c
Rui Monteiro <ruiseixas@yahoo.com>
2026-07-22 18:58:58 +0100

    Working fine

--- tests/overlapping_notes.py
+++ tests/overlapping_notes.py
@@ -1,7 +1,7 @@
 from jsonmidicreator import *
 
 settings << Device("loop")
-overlapping_notes = Note(1/2, "E") + Note(1/2, Beat(1), "E")
+overlapping_notes = Note(1/2, "E") + Note(Dotted(1/1), Beat(1), "E")
 overlapping_notes.len() >> Print()
 
 overlapping_notes >> Plot()
