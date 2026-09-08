4577f36194e832c34ee298696c5cf2089fbcac23
Rui Monteiro <ruiseixas@yahoo.com>
2026-09-08 20:48:01 +0100

    Added AI save

--- AI/four_notes.py
+++ AI/four_notes.py
@@ -1,4 +1,4 @@
 from jsonmidicreator import *
 
-four_notes = Clip() << Line("n:1/4, :1/4, :1/4, :1/4") >> Plot()
+four_notes = Clip() << Line("n:1/4, :1/4, :1/4, :1/4") >> Save("AI/four_notes.mid")
 
