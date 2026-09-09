c2421998af23f61bd111d114735a129e064b0c7b
Rui Monteiro <ruiseixas@yahoo.com>
2026-09-09 17:28:00 +0100

    Need to check the Load issue

--- AI/four_notes.py
+++ AI/four_notes.py
@@ -2,3 +2,5 @@ from jsonmidicreator import *
 
 four_notes = Clip() << Line("n:1/4, :1/4, :1/4, :1/4") >> Save("AI/four_notes.json")
 
+loaded_notes = Load("AI/four_notes.json")
+loaded_notes >> Plot()
---
+++ AI/inputs/some_notes.py
@@ -0,0 +1,5 @@
+from jsonmidicreator import *
+
+some_notes = Load("AI/inputs/some_notes.json")
+some_notes >> Plot()
+
