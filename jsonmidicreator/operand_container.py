8f19fc2ddb821925034b948d346711883dfef5ec
Rui Monteiro <ruiseixas@yahoo.com>
2026-09-09 12:27:11 +0100

    Improved code to process Clip mask

--- jsonmidicreator/operand_container.py
+++ jsonmidicreator/operand_container.py
@@ -429,9 +429,14 @@ class Container(o.Operand):
                         # Finally adds the decomposed elements to the Container stack
                         self._extend(operand._data)
                     case _: # operand is a Pipe
-                        for single_item in self.unmasked_items():
+                        items_list = self._items
+                        if isinstance(self, Clip):
+                            items_list = self.elements_unmasked()
+                        for single_item in items_list:
                             if isinstance (single_item, o.Operand):
                                 single_item << operand.copy()   # Avoids the share of a single parameter
+                            else:
+                                single_item << operand
 
             case od.Serialization():
                 self.loadSerialization( operand.getSerialization() )
@@ -462,14 +467,11 @@ class Container(o.Operand):
                     single_parameter = operand.chaoticize()
                     single_item << single_parameter
             case _:
+                items_list = self._items
                 if isinstance(self, Clip):
-                    for single_element in self.elements_unmasked():
-                        if isinstance (single_element, o.Operand):
-                            single_element << operand
-                else:
-                    for single_item in self._items:
-                        if isinstance (single_item, o.Operand):
-                            single_item << operand
+                    items_list = self.elements_unmasked()
+                for single_item in items_list:
+                    single_item << operand
         return self
 
 
