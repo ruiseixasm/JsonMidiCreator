8a752d98a87400eb02c90d16a5f8eccd5e9453fe
Rui Monteiro <ruiseixas@yahoo.com>
2026-09-09 03:41:15 +0100

    Comments

--- jsonmidicreator/operand_generic.py
+++ jsonmidicreator/operand_generic.py
@@ -2905,7 +2905,7 @@ class Process(Generic):
         match operand:
             case oc.Composition() | oe.Element():
                 if isinstance(operand, oc.Composition) and operand.len() == 0:
-                    return playlist # exists with nothing right away
+                    return playlist # exits with nothing right away
                 # Generates the Clock data regardless, needed for correct JsonMidiPlayer processing
                 clock_length: ra.Length = (operand.finish() % ra.Length()).roundMeasures()
                 default_clock: oe.Clock = settings % oe.Clock()
