b229312c67ae869c0556162d56f6c5f558338759
Rui <ruiseixas@yahoo.com>
2026-09-10 22:52:13 +0100

    Not working well

--- Instruments/Hybrid/three_notes.py
+++ Instruments/Hybrid/three_notes.py
@@ -1,6 +1,6 @@
 from jsonmidicreator import *
 
-RD_Hybrid.program_change(1, 27) >> Play()   # Sets the instrument to 27. Steel Galloper
+RD_Hybrid.program_change(27, 1) >> Play()   # Sets the instrument to 27. Steel Galloper
 
 three_notes = Clip(Line(
     "n:1/2:C5, :1/4:D5, ::D#5"
