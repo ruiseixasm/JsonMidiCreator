b4cf393576c18cff66f099e23478b4cf7ea26fc1
Rui <ruiseixas@yahoo.com>
2026-09-10 23:15:25 +0100

    Placement of TimeSignature is crytical

--- Instruments/Hybrid/three_notes.py
+++ Instruments/Hybrid/three_notes.py
@@ -2,9 +2,9 @@ from jsonmidicreator import *
 
 RD_Hybrid.program_change(27, 1) >> Play()   # Sets the instrument to 27. Steel Galloper
 
-three_notes = Clip(Line(
+three_notes = Clip(TimeSignature(7, 4), Name("Riffle"), Line(
     "n:3b:C5, :2b:D5, ::D#5"
-), Name("Riffle")) << TimeSignature(7, 4)
+))
 
 three_notes /= 4
 
