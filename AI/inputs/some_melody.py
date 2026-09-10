248f82f06e3f1fea30a0d91b96394eb6ed282c00
Rui <ruiseixas@yahoo.com>
2026-09-10 11:12:18 +0100

    Try

---
+++ AI/inputs/some_melody.py
@@ -0,0 +1,5 @@
+from jsonmidicreator import *
+
+some_notes = Load("AI/inputs/some_melody.json")
+some_notes >> Plot()
+
---
+++ AI/inputs/try_melody.py
@@ -0,0 +1,5 @@
+from jsonmidicreator import *
+
+some_notes = Load("AI/inputs/try_melody.json")
+some_notes >> Plot()
+
