4fb0ab2881648ef0b1f74f77f527ae365530445a
Rui <ruiseixas@yahoo.com>
2026-09-10 22:48:41 +0100

    Added instrument

---
+++ Instruments/Hybrid/three_notes.py
@@ -0,0 +1,9 @@
+from jsonmidicreator import *
+
+RD_Hybrid.program_change(1, 27) >> Play()   # Sets the instrument to 27. Steel Galloper
+
+three_notes = Clip(Line(
+    "n:1/2:C5, :1/4:D5, ::D#5"
+))
+
+three_notes >> Plot()
--- jsonmidicreator/resource_devices.py
+++ jsonmidicreator/resource_devices.py
@@ -36,7 +36,7 @@ from . import operand_chaos as ch
 
 
 
-class RD_Blofeld:
+class RD_Hybrid:
 
     device          = od.Device("Blofeld")
 
@@ -45,7 +45,7 @@ class RD_Blofeld:
 
     def program_change(sound: int, bank: str | int = "A") -> oe.ProgramChange:
         if isinstance(bank, str):
-            bank = ou.Bank(RD_Blofeld.banks[bank.strip().upper()])
+            bank = ou.Bank(RD_Hybrid.banks[bank.strip().upper()])
         return oe.ProgramChange(sound, ou.Bank(bank))
 
     # A total of 8 banks
@@ -64,7 +64,7 @@ class RD_Blofeld:
     def control_change(parameter: str = "Cutoff", group: str = "FILTER 1") -> oe.ControlChange:
         parameter = parameter.strip()
         group = group.strip().upper()
-        return oe.ControlChange(RD_Blofeld.midi_cc[group][parameter])
+        return oe.ControlChange(RD_Hybrid.midi_cc[group][parameter])
 
     midi_cc: dict[str,
                 dict[ str, dict[str, int] ]
@@ -1103,4 +1103,220 @@ class RD_UnoSynth:
                 }
             }
 
+
+
+class RD_Hybrid:
+
+    device          = od.Device("loop")
+
+    # Activate "Ctrl Receive" in "Shift + Global" and turn the data knob to select it on Global MIDI
+
+
+    def program_change(sound: int, bank: str | int = "A") -> oe.ProgramChange:
+        if isinstance(bank, str):
+            bank = ou.Bank(RD_Hybrid.banks[bank.strip().upper()])
+        return oe.ProgramChange(sound, ou.Bank(bank))
+
+    # A total of 8 banks
+    banks: dict[str, int] = {
+        "A":    1,
+        "B":    2,
+        "C":    3,
+        "D":    4,
+        "E":    5,
+        "F":    6,
+        "G":    7,
+        "H":    8
+    }
+
+
+    def control_change(parameter: str = "Cutoff", group: str = "FILTER 1") -> oe.ControlChange:
+        parameter = parameter.strip()
+        group = group.strip().upper()
+        return oe.ControlChange(RD_Hybrid.midi_cc[group][parameter])
+
+    midi_cc: dict[str,
+                dict[ str, dict[str, int] ]
+            ] = {
+
+                # Controllers
+                "CONTROLLERS": {
+                    "Modulation":   { "NUMBER": 1 },
+                    "Breath":       { "NUMBER": 2 },
+                    "Foot":         { "NUMBER": 4 },
+                    "Sustain":      { "NUMBER": 64 }
+                },
+
+                # Oscillators
+                "OSC COMMON": {
+                    "Sync":         { "NUMBER": 49 },
+                    "Pitchmod":     { "NUMBER": 50 },
+                    "Glide Active": { "NUMBER": 65 },
+                    "Glide Mode":   { "NUMBER": 51 },
+                    "Glide Rate":   { "NUMBER": 5 }
+                },
+                "OSC 1": {
+                    "Octave":       { "NUMBER": 63 },
+                    "Semitone":     { "NUMBER": 64 },
+                    "Detune":       { "NUMBER": 29 },
+                    "FM":           { "NUMBER": 30 },
+                    "Shape":        { "NUMBER": 31 },
+                    "PW":           { "NUMBER": 33 },
+                    "PWM":          { "NUMBER": 34 }
+                },
+                "OSC 2": {
+                    "Octave":       { "NUMBER": 35 },
+                    "Semitone":     { "NUMBER": 36 },
+                    "Detune":       { "NUMBER": 37 },
+                    "FM":           { "NUMBER": 38 },
+                    "Shape":        { "NUMBER": 39 },
+                    "PW":           { "NUMBER": 40 },
+                    "PWM":          { "NUMBER": 41 }
+                },
+                "OSC 3": {
+                    "Octave":       { "NUMBER": 42 },
+                    "Semitone":     { "NUMBER": 43 },
+                    "Detune":       { "NUMBER": 44 },
+                    "FM":           { "NUMBER": 45 },
+                    "Shape":        { "NUMBER": 46 },
+                    "PW":           { "NUMBER": 47 },
+                    "PWM":          { "NUMBER": 48 }
+                },
+
+                # Noise
+                "NOISE": {
+                    "Colour":       { "NUMBER": 62 }
+                },
+
+                # Filters
+                "FILTER 1": {
+                    "Type":         { "NUMBER": 68 },
+                    "Cutoff":       { "NUMBER": 69 },
+                    "Resonance":    { "NUMBER": 70 },
+                    "Drive":        { "NUMBER": 71 },
+                    "Keytrack":     { "NUMBER": 72 },
+                    "Env Amount":   { "NUMBER": 73 },
+                    "Env Velocity": { "NUMBER": 74 },
+                    "Mod":          { "NUMBER": 75 },
+                    "FM":           { "NUMBER": 76 },
+                    "Pan":          { "NUMBER": 77 },
+                    "Panmod":       { "NUMBER": 78 }
+                },
+                "FILTER COMMON": {
+                    "Routing":      { "NUMBER": 67 }
+                },
+                "FILTER 2": {
+                    "Type":         { "NUMBER": 79 },
+                    "Cutoff":       { "NUMBER": 80 },
+                    "Resonance":    { "NUMBER": 81 },
+                    "Drive":        { "NUMBER": 82 },
+                    "Keytrack":     { "NUMBER": 83 },
+                    "Env Amount":   { "NUMBER": 84 },
+                    "Env Velocity": { "NUMBER": 85 },
+                    "Mod":          { "NUMBER": 86 },
+                    "FM":           { "NUMBER": 87 },
+                    "Pan":          { "NUMBER": 88 },
+                    "Panmod":       { "NUMBER": 89 }
+                },
+
+                # Envelopes
+                "FILTER ENV": {
+                    "Attack":       { "NUMBER": 95 },
+                    "Decay":        { "NUMBER": 96 },
+                    "Sustain":      { "NUMBER": 97 },
+                    "Decay 2":      { "NUMBER": 98 },
+                    "Sustain 2":    { "NUMBER": 99 },
+                    "Release":      { "NUMBER": 100 }
+                },
+                "AMP ENV": {
+                    "Attack":       { "NUMBER": 101 },
+                    "Decay":        { "NUMBER": 102 },
+                    "Sustain":      { "NUMBER": 103 },
+                    "Decay 2":      { "NUMBER": 104 },
+                    "Sustain 2":    { "NUMBER": 105 },
+                    "Release":      { "NUMBER": 106 }
+                },
+                "ENV 3": {
+                    "Attack":       { "NUMBER": 107 },
+                    "Decay":        { "NUMBER": 108 },
+                    "Sustain":      { "NUMBER": 109 },
+                    "Decay 2":      { "NUMBER": 110 },
+                    "Sustain 2":    { "NUMBER": 111 },
+                    "Release":      { "NUMBER": 112 }
+                },
+                "ENV 4": {
+                    "Attack":       { "NUMBER": 113 },
+                    "Decay":        { "NUMBER": 114 },
+                    "Sustain":      { "NUMBER": 115 },
+                    "Decay 2":      { "NUMBER": 116 },
+                    "Sustain 2":    { "NUMBER": 117 },
+                    "Release":      { "NUMBER": 118 }
+                },
+
+                # LFOs
+                "LFO 1": {
+                    "Shape":        { "NUMBER": 15 },
+                    "Speed":        { "NUMBER": 16 },
+                    "Sync":         { "NUMBER": 17 },
+                    "Delay":        { "NUMBER": 18 }
+                },
+                "LFO 2": {
+                    "Shape":        { "NUMBER": 19 },
+                    "Speed":        { "NUMBER": 20 },
+                    "Sync":         { "NUMBER": 21 },
+                    "Delay":        { "NUMBER": 22 }
+                },
+                "LFO 3": {
+                    "Shape":        { "NUMBER": 23 },
+                    "Speed":        { "NUMBER": 24 },
+                    "Sync":         { "NUMBER": 25 },
+                    "Delay":        { "NUMBER": 26 }
+                },
+
+                # Amplifier
+                "AMP COMMON": {
+                    "Volume":       { "NUMBER": 90 },
+                    "Velocity":     { "NUMBER": 91 },
+                    "Mod":          { "NUMBER": 92 }
+                },
+
+                # Effects
+                "EFFECTS": {
+                    "Mix 1":        { "NUMBER": 93 },
+                    "Mix 2":        { "NUMBER": 94 }
+                },
+
+                # Arpeggiator
+                "ARPEGGIATOR": {
+                    "Octave":       { "NUMBER": 12 },
+                    "Length":       { "NUMBER": 13 },
+                    "Mode":         { "NUMBER": 14 }
+                },
+
+                # Mixers
+                "MIXER COMMON": {
+                    "Volume":       { "NUMBER": 7 },
+                    "Pan":          { "NUMBER": 10 }
+                },
+                "MIXER OSC 1": {
+                    "Level":        { "NUMBER": 52 },
+                    "Filter":       { "NUMBER": 53 }
+                },
+                "MIXER OSC 2": {
+                    "Level":        { "NUMBER": 56 },
+                    "Filter":       { "NUMBER": 57 }
+                },
+                "MIXER OSC 3": {
+                    "Level":        { "NUMBER": 58 },
+                    "Filter":       { "NUMBER": 59 }
+                },
+                "MIXER RING": {
+                    "Level":        { "NUMBER": 54 },
+                    "Filter":       { "NUMBER": 55 }
+                },
+                "MIXER NOISE": {
+                    "Level":        { "NUMBER": 60 },
+                    "Filter":       { "NUMBER": 61 }
+                }
+            }
 
