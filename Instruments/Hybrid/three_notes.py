5b799da9d4a6fb0011b53c9cd6efd855d3d7387a
Rui <ruiseixas@yahoo.com>
2026-09-12 09:16:59 +0100

    Saved with multiple tempos

--- Instruments/Hybrid/three_notes.py
+++ Instruments/Hybrid/three_notes.py
@@ -1,6 +1,6 @@
 from jsonmidicreator import *
 
-settings << ClockedDevices(["loop"]) << [Tempo(100.5)]
+settings << ClockedDevices(["loop"]) << [Tempo(100.5), Tempo(100.5, Measure(2)), Tempo(140, Measure(2))]
 
 RD_Hybrid.program_change(27, 1) >> Play()   # Sets the instrument to 27. Steel Galloper
 
@@ -10,4 +10,4 @@ three_notes = Clip(TimeSignature(7, 4), Name("Riffle"), Line(
 
 three_notes *= 4
 
-three_notes * [1, 2] >> Export("Instruments/Hybrid/three_notes.json") >> Play(True)
+three_notes * [1, 2] * 2 >> Export("Instruments/Hybrid/three_notes.json") >> Play(True)
