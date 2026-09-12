066e99df26bef22b3bb7e1b0265bd76e099fcf22
Rui Monteiro <ruiseixas@yahoo.com>
2026-09-12 18:01:44 +0100

    Adjusted how Tempo is set

--- Instruments/Hybrid/three_notes.py
+++ Instruments/Hybrid/three_notes.py
@@ -1,12 +1,19 @@
 from jsonmidicreator import *
 
-settings << ClockedDevices(["loop"]) << [Tempo(100.5), Tempo(100.5, Measure(2)), Tempo(200, Measure(2))]
+settings << ClockedDevices(["loop"])
 
 RD_Hybrid.program_change(27, 1) >> Play()   # Sets the instrument to 27. Steel Galloper
 
 three_notes = Clip(TimeSignature(7, 4), Name("Riffle"), Line(
     "n:3b:C5, :2b:D5, ::D#5"
 ))
+
+settings << [
+    Tempo(100.5),
+    Tempo(100.5, Measure(three_notes, 2)),
+    Tempo(200, Measure(three_notes, 2))
+]
+
 
 three_notes *= 4
 
