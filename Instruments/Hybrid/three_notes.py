90c0d7dcd63370109f44ba570b284d4b7840f66e
Rui <ruiseixas@yahoo.com>
2026-09-13 16:55:48 +0100

    Gets direct acces to fraction

--- Instruments/Hybrid/three_notes.py
+++ Instruments/Hybrid/three_notes.py
@@ -2,11 +2,11 @@ from jsonmidicreator import *
 
 settings << ClockedDevices(["loop"])
 
-RD_Hybrid.program_change(27, 1) >> Play()   # Sets the instrument to 27. Steel Galloper
-
 three_notes = Clip(TimeSignature(7, 4), Name("Riffle"), Line(
     "n:3b:C5, :2b:D5, ::D#5"
 ))
+
+three_notes += RD_Hybrid.program_change(27, 1)   # Sets the instrument to 27. Steel Galloper
 
 settings << [
     Tempo(100.5),
@@ -17,4 +17,4 @@ settings << [
 
 three_notes *= 4
 # In order to the following `.json` file be created in the same folder, run Python in the folder
-three_notes * [1, 2] * 2 >> Export("three_notes.json") >> Play(verbose=True)
+three_notes * [1, 2] * 2 >> Export("three_notes.json") >> Play(loop=2, verbose=True)
