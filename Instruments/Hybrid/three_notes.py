a809159f769d705afb39b6d16fc0144fafd6aa9c
Rui <ruiseixas@yahoo.com>
2026-09-15 00:49:35 +0100

    Testing script

--- Instruments/Hybrid/three_notes.py
+++ Instruments/Hybrid/three_notes.py
@@ -9,12 +9,14 @@ three_notes = Clip(TimeSignature(7, 4), Name("Riffle"), Line(
 three_notes += RD_Devices.Clip_program_change(27, 1)   # Sets the instrument to 27. Steel Galloper
 
 settings << [
-    Tempo(100.5),
-    Tempo(100.5, Measure(three_notes, 2)),
-    Tempo(200, Measure(three_notes, 2))
+    Tempo(30),
+    Tempo(90, three_notes * 4 % Length())
 ]
 
 
 three_notes *= 4
 # In order to the following `.json` file be created in the same folder, run Python in the folder
 three_notes * [1, 2] * 2 >> Export("three_notes.json") >> Play(loop=1, verbose=True)
+
+# If the total duration for a loop is 28 seconds then the interpolation is working fine.
+
