d4f01d4648c6aeef657a7667c5b132d5411f3645
Rui <ruiseixas@yahoo.com>
2026-09-18 20:43:44 +0100

    Better documented element replacement

--- jsonmidicreator/operand_element.py
+++ jsonmidicreator/operand_element.py
@@ -428,24 +428,27 @@ class Element(o.Operand):
                         if not self._owner_clip._set:
                             if operand:
                                 position_offset: ra.Position = self.start() - operand[0].start()
-                                elements_list: list[Element] = [
+                                replacing_elements_list: list[Element] = [
                                     element.copy()._set_owner_clip(self._owner_clip) for element in operand
                                 ]
-                                self._owner_clip._extend(elements_list)
+                                self._owner_clip._extend(replacing_elements_list)
                             self._owner_clip._set = True
                         self._owner_clip._remove(self, True)
+            
             case oc.Clip():
+                # Replace this element by the Clip elements
                 if self._owner_clip is not None:
                     if not self._owner_clip._set:
                         if operand.len() > 0:
                             position_offset: ra.Position = self.start() - operand.start()
-                            elements_list: list[Element] = [
+                            replacing_elements_list: list[Element] = [
+                                # Implicit copy
                                 (element + position_offset)._set_owner_clip(self._owner_clip)
-                                for element in operand.elements_unmasked()
+                                for element in operand._items
                             ]
-                            self._owner_clip._extend(elements_list)
+                            self._owner_clip._extend(replacing_elements_list)
                         self._owner_clip._set = True
-                    self._owner_clip._remove(self, True)
+                    self._owner_clip._remove(self, True)    # Ultimately, replaces it with nothing
 
             case ou.Masked():
                 self._masked                = operand._unit != 0
