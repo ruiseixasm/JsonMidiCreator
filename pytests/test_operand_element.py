03f5f37c7b30261e5e67ee4c20b9ee16b3059733
ruiseixasm <ruiseixas@yahoo.com>
2026-09-26 00:40:52 +0100

    Added test_note_duration unit test

--- pytests/test_operand_element.py
+++ pytests/test_operand_element.py
@@ -410,6 +410,20 @@ def test_note_position():
 # test_note_position()
 
 
+def test_note_duration():
+    note = Note()
+    assert note % Position() == 0.0
+
+    note << Duration(1/2)
+    assert note % Position() == 0.0
+    note << Duration(1/4)
+
+    note << Right(Duration(1/2))
+    assert note % Position() == 0.0
+
+# test_note_duration()
+
+
 def test_note_pitch():
 
     settings << None
