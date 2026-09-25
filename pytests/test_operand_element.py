ffa2aa6b0d59ec41c8ed23ea5bf8a6b92627d180
ruiseixasm <ruiseixas@yahoo.com>
2026-09-26 00:43:55 +0100

    Passes test

--- pytests/test_operand_element.py
+++ pytests/test_operand_element.py
@@ -420,6 +420,11 @@ def test_note_duration():
 
     note << Right(Duration(1/2))
     assert note % Position() == 0.0
+    note << Duration(1/4)
+
+    note << Left(Duration(1/2)) # Can't expand negatively
+    assert note % Position() == NoteValue(-1/4)
+    note << Duration(1/4)
 
 # test_note_duration()
 
