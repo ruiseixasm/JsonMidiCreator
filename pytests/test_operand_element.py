a3550fb921d9183b8912a81952f0a8330eccc816
Rui <ruiseixas@yahoo.com>
2026-09-26 02:23:57 +0100

    Improved test_note_duration

--- pytests/test_operand_element.py
+++ pytests/test_operand_element.py
@@ -411,21 +411,24 @@ def test_note_position():
 
 
 def test_note_duration():
+
+    settings << None
+
     note = Note()
     assert note % Position() == 0.0
 
     note << Duration(1/2)
     assert note % Position() == 0.0
-    note >> Plot()
+    # note >> Plot()
     note << Duration(1/4)
 
     note << RightDuration(1/2)  # Can expand negatively
     assert note % Position() == -NoteValue(1/4)
-    note >> Plot()
+    # note >> Plot()
     note -= RightDuration(1/4)
     assert note % Position() == 0.0
 
-test_note_duration()
+# test_note_duration()
 
 
 def test_note_pitch():
